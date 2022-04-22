1168de3b1c7efd96ff372998ea2cccf0d903b222
Tom <tom@vegaprotocol.io>
2022-04-22 13:08:47 +0200

    feat: Adding dynamic port assignment (#17)
    * feat: Moving to dynamic ports, the Vega server will now determine port choice from the available set, allowing multiple servers to run alongside one another.

--- vega_sim/null_service.py
+++ vega_sim/null_service.py
@@ -2,28 +2,232 @@ import atexit
 import logging
 import shutil
 import signal
+import socket
 import subprocess
 import tempfile
 import time
+from collections import namedtuple
+from contextlib import closing
+from enum import Enum, auto
 from multiprocessing import Process
 from os import path
-from typing import List, Optional
+from typing import Dict, List, Optional, Set
 
 import requests
 import toml
 from urllib3.exceptions import MaxRetryError
 
-from vega_sim import vega_home_path, vega_bin_path
-from vega_sim.constants import (
-    DATA_NODE_GRPC_PORT,
-    DATA_NODE_REST_PORT,
-    FAUCET_PORT,
-    WALLET_DEFAULT_PORT,
-    VEGA_NODE_PORT,
-)
+from vega_sim import vega_bin_path, vega_home_path
+
 from vega_sim.service import VegaService
 
 logger = logging.getLogger(__name__)
+
+PortUpdateConfig = namedtuple(
+    "PortUpdateConfig", ["file_path", "config_path", "key", "val_func"]
+)
+
+
+class Ports(Enum):
+    DATA_NODE_GRPC = auto()
+    DATA_NODE_REST = auto()
+    DATA_NODE_GRAPHQL = auto()
+    FAUCET = auto()
+    WALLET = auto()
+    VEGA_NODE = auto()
+    CORE_GRPC = auto()
+    CORE_REST = auto()
+    BROKER = auto()
+    METRICS = auto()
+    PPROF = auto()
+    CONSOLE = auto()
+
+
+PORT_UPDATERS = {
+    Ports.DATA_NODE_GRPC: [
+        PortUpdateConfig(
+            ("config", "data-node", "config.toml"),
+            ["API"],
+            "Port",
+            lambda port: port,
+        ),
+        PortUpdateConfig(
+            ("config", "data-node", "config.toml"),
+            ["Gateway", "Node"],
+            "Port",
+            lambda port: port,
+        ),
+        PortUpdateConfig(
+            ("config", "wallet-service", "networks", "local.toml"),
+            ["API", "GRPC"],
+            "Hosts",
+            lambda port: [f"localhost:{port}"],
+        ),
+    ],
+    Ports.DATA_NODE_REST: [
+        PortUpdateConfig(
+            ("config", "data-node", "config.toml"),
+            ["Gateway", "REST"],
+            "Port",
+            lambda port: port,
+        ),
+        PortUpdateConfig(
+            ("config", "wallet-service", "networks", "local.toml"),
+            ["API", "REST"],
+            "Hosts",
+            lambda port: [f"localhost:{port}"],
+        ),
+    ],
+    Ports.FAUCET: [
+        PortUpdateConfig(
+            ("config", "faucet", "config.toml"), [], "Port", lambda port: port
+        ),
+    ],
+    Ports.WALLET: [
+        PortUpdateConfig(
+            ("config", "wallet-service", "networks", "local.toml"),
+            [],
+            "Port",
+            lambda port: port,
+        ),
+    ],
+    Ports.VEGA_NODE: [
+        PortUpdateConfig(
+            ("config", "node", "config.toml"),
+            ["Blockchain", "Null"],
+            "Port",
+            lambda port: port,
+        ),
+    ],
+    Ports.DATA_NODE_GRAPHQL: [
+        PortUpdateConfig(
+            ("config", "data-node", "config.toml"),
+            ["Gateway", "GraphQL"],
+            "Port",
+            lambda port: port,
+        ),
+        PortUpdateConfig(
+            ("config", "wallet-service", "networks", "local.toml"),
+            ["API", "GraphQL"],
+            "Hosts",
+            lambda port: [f"localhost:{port}"],
+        ),
+    ],
+    Ports.CORE_GRPC: [
+        PortUpdateConfig(
+            ("config", "faucet", "config.toml"),
+            ["Node"],
+            "Port",
+            lambda port: port,
+        ),
+        PortUpdateConfig(
+            ("config", "node", "config.toml"),
+            ["API"],
+            "Port",
+            lambda port: port,
+        ),
+        PortUpdateConfig(
+            ("config", "data-node", "config.toml"),
+            ["API"],
+            "CoreNodeGRPCPort",
+            lambda port: port,
+        ),
+    ],
+    Ports.CORE_REST: [
+        PortUpdateConfig(
+            ("config", "node", "config.toml"),
+            ["API", "REST"],
+            "Port",
+            lambda port: port,
+        ),
+    ],
+    Ports.BROKER: [
+        PortUpdateConfig(
+            ("config", "data-node", "config.toml"),
+            ["Broker", "SocketConfig"],
+            "Port",
+            lambda port: port,
+        ),
+        PortUpdateConfig(
+            ("config", "node", "config.toml"),
+            ["Broker", "Socket"],
+            "Port",
+            lambda port: port,
+        ),
+    ],
+    Ports.METRICS: [
+        PortUpdateConfig(
+            ("config", "data-node", "config.toml"),
+            ["Metrics"],
+            "Port",
+            lambda port: port,
+        ),
+        PortUpdateConfig(
+            ("config", "node", "config.toml"),
+            ["Metrics"],
+            "Port",
+            lambda port: port,
+        ),
+    ],
+    Ports.PPROF: [
+        PortUpdateConfig(
+            ("config", "data-node", "config.toml"),
+            ["Pprof"],
+            "Port",
+            lambda port: port,
+        ),
+        PortUpdateConfig(
+            ("config", "node", "config.toml"),
+            ["Pprof"],
+            "Port",
+            lambda port: port,
+        ),
+    ],
+    Ports.CONSOLE: [
+        PortUpdateConfig(
+            ("config", "wallet-service", "networks", "local.toml"),
+            ["Console"],
+            "LocalPort",
+            lambda port: port,
+        ),
+    ],
+}
+
+
+class VegaStartupTimeoutError(Exception):
+    pass
+
+
+class ServiceNotStartedError(Exception):
+    pass
+
+
+class SocketNotFoundError(Exception):
+    pass
+
+
+def _find_free_port(existing_set: Optional[Set[int]] = None):
+    ret_sock = 0
+    existing_set = (
+        existing_set.union(set([ret_sock]))
+        if existing_set is not None
+        else set([ret_sock])
+    )
+
+    num_tries = 0
+    while ret_sock in existing_set:
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+            s.bind(("", 0))
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            ret_sock = s.getsockname()[1]
+
+        num_tries += 1
+        if num_tries >= 100:
+            # Arbitrary high number. If we try 100 times and fail to find
+            # a port it seems reasonable to give up
+            raise SocketNotFoundError("Failed finding a free socket")
+
+    return ret_sock
 
 
 def _popen_process(
@@ -37,15 +241,34 @@ def _popen_process(
     return sub_proc
 
 
-def _update_node_config(vega_home: str) -> None:
+def _update_node_config(vega_home: str, port_config: Dict[Ports, int]) -> None:
     config_path = path.join(vega_home, "config", "node", "config.toml")
     config_toml = toml.load(config_path)
     config_toml["Blockchain"]["Null"]["GenesisFile"] = path.join(
         vega_home, "genesis.json"
     )
 
+    existing_ports = set(port_config.values())
+    for port in Ports:
+        if port in port_config:
+            continue
+        new_port = _find_free_port(existing_ports)
+        existing_ports.add(new_port)
+        port_config[port] = new_port
     with open(config_path, "w") as f:
         toml.dump(config_toml, f)
+
+    for port_key, update_configs in PORT_UPDATERS.items():
+        for config in update_configs:
+            file_path = path.join(vega_home, *config.file_path)
+            config_toml = toml.load(file_path)
+            elem = config_toml
+            for k in config.config_path:
+                elem = elem[k]
+            elem[config.key] = config.val_func(port_config[port_key])
+
+            with open(file_path, "w") as f:
+                toml.dump(config_toml, f)
 
 
 def manage_vega_processes(
@@ -53,15 +276,16 @@ def manage_vega_processes(
     data_node_path: str,
     vega_wallet_path: str,
     run_wallet_with_console: bool = False,
-    run_wallet_with_token_dapp: bool = False,
+    port_config: Optional[Dict[Ports, int]] = None,
 ) -> None:
+    port_config = port_config if port_config is not None else {}
     with tempfile.TemporaryDirectory() as tmp_vega_dir:
         print(tmp_vega_dir)
         logger.debug(f"Running NullChain from vegahome of {tmp_vega_dir}")
         shutil.copytree(vega_home_path, f"{tmp_vega_dir}/vegahome")
 
         tmp_vega_home = tmp_vega_dir + "/vegahome"
-        _update_node_config(tmp_vega_home)
+        _update_node_config(tmp_vega_home, port_config=port_config)
 
         dataNodeProcess = _popen_process(
             [data_node_path, "node", "--home=" + tmp_vega_home],
@@ -100,12 +324,6 @@ def manage_vega_processes(
             "--home=" + tmp_vega_home,
             "--automatic-consent",
         ]
-        if run_wallet_with_console:
-            wallet_args += ["--with-console"]
-        if run_wallet_with_token_dapp:
-            wallet_args += ["--with-token-dapp"]
-        if run_wallet_with_token_dapp or run_wallet_with_console:
-            wallet_args += ["--no-browser"]
 
         vegaWalletProcess = _popen_process(
             wallet_args,
@@ -132,25 +350,12 @@ def manage_vega_processes(
             process.kill()
 
 
-class VegaStartupTimeoutError(Exception):
-    pass
-
-
-class ServiceNotStartedError(Exception):
-    pass
-
-
 class VegaServiceNull(VegaService):
     def __init__(
         self,
         vega_path: Optional[str] = None,
         data_node_path: Optional[str] = None,
         vega_wallet_path: Optional[str] = None,
-        wallet_port: int = WALLET_DEFAULT_PORT,
-        data_node_rest_port: int = DATA_NODE_REST_PORT,
-        data_node_grpc_port: int = DATA_NODE_GRPC_PORT,
-        vega_node_port: int = VEGA_NODE_PORT,
-        faucet_port: Optional[int] = FAUCET_PORT,
         start_immediately: bool = False,
         run_wallet_with_console: bool = False,
         run_wallet_with_token_dapp: bool = False,
@@ -161,11 +366,35 @@ class VegaServiceNull(VegaService):
         self.vega_wallet_path = vega_wallet_path or path.join(
             vega_bin_path, "vegawallet"
         )
-        self.wallet_port = wallet_port
-        self.data_node_rest_port = data_node_rest_port
-        self.data_node_grpc_port = data_node_grpc_port
-        self.faucet_port = faucet_port
-        self.vega_node_port = vega_node_port
+        self.wallet_port = 0
+        self.data_node_rest_port = 0
+        self.data_node_grpc_port = 0
+        self.data_node_graphql_port = 0
+        self.faucet_port = 0
+        self.vega_node_port = 0
+        self.console_port = 0
+        for port_opt in [
+            "wallet_port",
+            "data_node_grpc_port",
+            "data_node_rest_port",
+            "data_node_graphql_port",
+            "faucet_port",
+            "vega_node_port",
+            "console_port",
+        ]:
+            curr_ports = set(
+                [
+                    self.wallet_port,
+                    self.data_node_grpc_port,
+                    self.data_node_rest_port,
+                    self.data_node_graphql_port,
+                    self.faucet_port,
+                    self.vega_node_port,
+                    self.console_port,
+                ]
+            )
+            setattr(self, port_opt, _find_free_port(curr_ports))
+
         self.proc = None
         self.run_wallet_with_console = run_wallet_with_console
         self.run_wallet_with_token_dapp = run_wallet_with_token_dapp
@@ -184,18 +413,32 @@ class VegaServiceNull(VegaService):
                 "vega_path": self.vega_path,
                 "data_node_path": self.data_node_path,
                 "vega_wallet_path": self.vega_wallet_path,
-                "run_wallet_with_token_dapp": self.run_wallet_with_token_dapp,
                 "run_wallet_with_console": self.run_wallet_with_console,
+                "port_config": {
+                    Ports.WALLET: self.wallet_port,
+                    Ports.DATA_NODE_GRPC: self.data_node_grpc_port,
+                    Ports.DATA_NODE_REST: self.data_node_rest_port,
+                    Ports.FAUCET: self.faucet_port,
+                    Ports.VEGA_NODE: self.vega_node_port,
+                    Ports.CONSOLE: self.console_port,
+                    Ports.DATA_NODE_GRAPHQL: self.data_node_graphql_port,
+                },
             },
             daemon=True,
         )
         self.proc.start()
+
         # Wait for startup
         for _ in range(300):
             try:
                 requests.get(
                     f"http://localhost:{self.wallet_port}/api/v1/status"
                 ).raise_for_status()
+                if self.run_wallet_with_console:
+                    logger.info(
+                        "Vega Running. Launch console vs"
+                        f" {self.data_node_graphql_port} for GUI"
+                    )
                 return
             except (
                 MaxRetryError,
