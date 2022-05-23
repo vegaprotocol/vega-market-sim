e48306977cf359af5430cb47bac8882c973d6c59
Tom McLean <tom@vegaprotocol.io>
2022-05-23 18:59:10 +0100

    feat: Adding documentation

--- vega_sim/environment/environment.py
+++ vega_sim/environment/environment.py
@@ -1,3 +1,26 @@
+"""
+Module defining environments in which the Vega market sim can be interacted with by
+prespecified Agent classes. This enables a modular approach to simulating a
+market behaviour, be that with Agents with pre-specified strategies or
+those which learn through interaction with Vega itself.
+
+Currently there are two major formats of Environment, the MarketEnvironment
+and the MarketEnvironmentWithState. The crucial difference between these
+two is that in the *WithState version each agent recieves, at each timestep, a
+summary object representing the network's state at that time. This is
+intended to eventually speed up the step process overall as individual agents
+do not each need to gather state. Currently this 'state' object is a work-in-progress.
+
+The alternative environment, plain MarketEnvironment, passes the VegaService
+itself to agents at each timestep, allowing them to gather their own
+information. This allows more 'custom' agents but may be ultimately slower
+if the same information is retrieved multiple times.
+
+For examples of this setup, see examples/agent_market.
+
+"""
+
+
 import datetime
 import logging
 import random
@@ -29,6 +52,33 @@ class MarketEnvironment:
         transactions_per_block: int = 1,
         block_length_seconds: int = 1,
     ):
+        """Set up a Vega protocol environment with some specified agents.
+        Handles the entire Vega setup and environment lifetime process, allowing the
+        user to focus on building the Agents themselves.
+
+        Once an environment has been created, calling the 'run' function will
+        run a complete simulation of the environment lifecycle then shut down
+        the various Vega processes.
+
+        Args:
+            agents:
+                List[Agent], a list of instantiated Agent objects which will
+                    interact with the environment
+            n_steps:
+                int, The number of steps for the environment to run
+            random_agent_ordering:
+                bool, default True, In each step, whether the order of agent
+                    steps should be randomised. If False, the order of agents
+                    passed in will be used
+            transactions_per_block:
+                int, default 1, How many transactions should be contained
+                    for each block in the Vega chain. Often this is best set
+                    as the maximum number of actions agents can take per step
+                    to ensure they all happen 'at the same time' per step.
+            block_length_seconds:
+                int, default 1, How many seconds each block in the Vega chain
+                    is taken to represent.
+        """
         self.agents = agents
         self.n_steps = n_steps
         self.random_agent_ordering = random_agent_ordering
@@ -40,6 +90,18 @@ class MarketEnvironment:
         run_with_console: bool = False,
         pause_at_completion: bool = False,
     ) -> None:
+        """Run the simulation with specified agents.
+
+        Args:
+            run_with_console:
+                bool, default False, Whether the environment should attempt
+                    to spin up a full Vega console with which to observe
+                    the market behaviour
+            pause_at_completion:
+                bool, default False, If True will pause with a keypress-prompt
+                    once the simulation has completed, allowing the final state
+                    to be inspected, either via code or the Console
+        """
         with VegaServiceNull(
             run_wallet_with_console=run_with_console,
             warn_on_raw_data_access=False,
@@ -88,6 +150,36 @@ class MarketEnvironmentWithState(MarketEnvironment):
         transactions_per_block: int = 1,
         block_length_seconds: int = 1,
     ):
+        """Set up a Vega protocol environment with some specified agents.
+        Handles the entire Vega setup and environment lifetime process, allowing the
+        user to focus on building the Agents themselves.
+
+        Once an environment has been created, calling the 'run' function will
+        run a complete simulation of the environment lifecycle then shut down
+        the various Vega processes.
+
+        This class differs from MarketEnvironment in that each agent will only
+        receive a state representation object rather than a full VegaService.
+
+        Args:
+            agents:
+                List[StateAgent], a list of instantiated Agent objects which will
+                    interact with the environment
+            n_steps:
+                int, The number of steps for the environment to run
+            random_agent_ordering:
+                bool, default True, In each step, whether the order of agent
+                    steps should be randomised. If False, the order of agents
+                    passed in will be used
+            transactions_per_block:
+                int, default 1, How many transactions should be contained
+                    for each block in the Vega chain. Often this is best set
+                    as the maximum number of actions agents can take per step
+                    to ensure they all happen 'at the same time' per step.
+            block_length_seconds:
+                int, default 1, How many seconds each block in the Vega chain
+                    is taken to represent.
+        """
         super().__init__(
             agents=agents,
             n_steps=n_steps,
