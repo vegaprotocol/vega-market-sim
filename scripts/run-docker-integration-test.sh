c09a9bcfe4a194056a0d75b6412350cc9eaa3886
Tom McLean <tom@vegaprotocol.io>
2022-07-13 09:36:03 +0100

    test: capture test logs

--- scripts/run-docker-integration-test.sh
+++ scripts/run-docker-integration-test.sh
@@ -2,6 +2,6 @@ docker run \
     --mount type=bind,source=$PWD/tests,target=/vega_market_sim/tests \
     --platform linux/amd64 \
     --name vega_test \
-    vega_sim_test:latest pytest -v -m integration
+    vega_sim_test:latest pytest -s -v -m integration
 docker cp vega_test:/tmp ./test_logs/
 docker rm vega_test
