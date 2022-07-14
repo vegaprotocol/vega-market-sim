5a148372c64b1c60e38df8b8ded06e16108d2bba
Tom McLean <tom@vegaprotocol.io>
2022-07-14 15:06:03 +0100

    feat: Moving parameter test framework to vega_sim

---
+++ vega_sim/parameter_tests/parameter/configs.py
@@ -0,0 +1,101 @@
+from vega_sim.parameter_tests.parameter.experiment import SingleParameterExperiment
+from vega_sim.parameter_tests.parameter.loggers import (
+    ideal_market_maker_single_data_extraction,
+    target_stake_additional_data,
+    v1_ideal_mm_additional_data,
+    tau_scaling_additional_data,
+)
+from vega_sim.scenario.registry import IdealMarketMaker, IdealMarketMakerV2
+
+TARGET_STAKE_SCALING_FACTOR_IDEAL = SingleParameterExperiment(
+    name="StakeTargetScaling",
+    parameter_to_vary="market.stake.target.scalingFactor",
+    values=["0.5", "5", "50"],
+    scenario=IdealMarketMaker(
+        num_steps=100,
+        state_extraction_fn=ideal_market_maker_single_data_extraction(
+            additional_data_fns=[
+                v1_ideal_mm_additional_data,
+                target_stake_additional_data,
+            ]
+        ),
+    ),
+    runs_per_scenario=3,
+    additional_parameters_to_set=[
+        ("market.liquidity.targetstake.triggering.ratio", "1")
+    ],
+)
+
+TAU_SCALING_FACTOR_IDEAL = SingleParameterExperiment(
+    name="TauScaling",
+    parameter_to_vary="market.liquidity.probabilityOfTrading.tau.scaling",
+    values=["1", "10", "100"],
+    scenario=IdealMarketMaker(
+        num_steps=100,
+        state_extraction_fn=ideal_market_maker_single_data_extraction(
+            additional_data_fns=[
+                v1_ideal_mm_additional_data,
+                tau_scaling_additional_data,
+            ]
+        ),
+    ),
+    runs_per_scenario=3,
+    additional_parameters_to_set=[
+        ("market.liquidity.targetstake.triggering.ratio", "1")
+    ],
+)
+
+TARGET_STAKE_SCALING_FACTOR_IDEAL_v2 = SingleParameterExperiment(
+    name="StakeTargetScaling_v2",
+    parameter_to_vary="market.stake.target.scalingFactor",
+    values=["0.5", "5", "50"],
+    scenario=IdealMarketMakerV2(
+        initial_price=10,
+        step_length_seconds=60,
+        block_length_seconds=1,
+        buy_intensity=2,
+        sell_intensity=2,
+        kappa=1,
+        sigma=200,
+        num_steps=200,
+        state_extraction_fn=ideal_market_maker_single_data_extraction(
+            additional_data_fns=[
+                target_stake_additional_data,
+            ]
+        ),
+    ),
+    runs_per_scenario=20,
+    additional_parameters_to_set=[
+        ("market.liquidity.targetstake.triggering.ratio", "1")
+    ],
+)
+
+TAU_SCALING_FACTOR_IDEAL_v2 = SingleParameterExperiment(
+    name="TauScaling_v2",
+    parameter_to_vary="market.liquidity.probabilityOfTrading.tau.scaling",
+    values=["1", "10", "100"],
+    scenario=IdealMarketMakerV2(
+        initial_price=10,
+        step_length_seconds=60,
+        block_length_seconds=1,
+        buy_intensity=2,
+        sell_intensity=2,
+        kappa=1,
+        sigma=200,
+        num_steps=200,
+        state_extraction_fn=ideal_market_maker_single_data_extraction(
+            additional_data_fns=[tau_scaling_additional_data]
+        ),
+    ),
+    runs_per_scenario=3,
+    additional_parameters_to_set=[
+        ("market.liquidity.targetstake.triggering.ratio", "1")
+    ],
+)
+
+CONFIGS = [
+    TARGET_STAKE_SCALING_FACTOR_IDEAL,
+    TAU_SCALING_FACTOR_IDEAL,
+    TAU_SCALING_FACTOR_IDEAL_v2,
+    TARGET_STAKE_SCALING_FACTOR_IDEAL_v2,
+]
---
+++ vega_sim/parameter_tests/parameter/experiment.py
@@ -0,0 +1,118 @@
+import csv
+from dataclasses import dataclass
+import json
+import os
+from typing import Any, Dict, List, Optional, Tuple
+import pathlib
+
+from vega_sim.scenario.scenario import Scenario
+from vega_sim.null_service import VegaServiceNull
+
+
+PARAMETER_AMEND_WALLET = ("param", "amend")
+
+FILE_PATTERN = "NETP_{param_name}_{param_value}.csv"
+OUTPUT_DIR = "results"
+
+
+@dataclass
+class Experiment:
+    name: str
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "name": self.name,
+        }
+
+
+@dataclass
+class SingleParameterExperiment(Experiment):
+    parameter_to_vary: str
+    values: List[str]
+    scenario: Scenario
+    runs_per_scenario: int = 1
+    additional_parameters_to_set: Optional[Dict[str, str]] = None
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "name": self.name,
+            "parameter_tested": self.parameter_to_vary,
+            "tested_values": self.values,
+            "scenario": self.scenario.__class__.__name__,
+            "num_runs": self.runs_per_scenario,
+            "additional_parameters": self.additional_parameters_to_set,
+        }
+
+
+def _run_parameter_iteration(
+    scenario: Scenario,
+    parameter_to_vary: str,
+    value: str,
+    additional_parameters_to_set: Optional[Dict[str, str]] = None,
+) -> Any:
+    with VegaServiceNull(warn_on_raw_data_access=False) as vega:
+        vega.create_wallet(*PARAMETER_AMEND_WALLET)
+        vega.mint(
+            PARAMETER_AMEND_WALLET[0],
+            asset="VOTE",
+            amount=1e4,
+        )
+
+        if additional_parameters_to_set is not None:
+            for param, new_value in additional_parameters_to_set.items():
+                vega.update_network_parameter(
+                    PARAMETER_AMEND_WALLET[0], parameter=param, new_value=new_value
+                )
+
+        vega.update_network_parameter(
+            PARAMETER_AMEND_WALLET[0], parameter=parameter_to_vary, new_value=value
+        )
+
+        return scenario.run_iteration(vega=vega)
+
+
+def run_single_parameter_experiment(
+    experiment: SingleParameterExperiment,
+) -> Dict[str, List[Any]]:
+    results = {}
+    for value in experiment.values:
+        results[value] = []
+        for _ in range(experiment.runs_per_scenario):
+            results[value].append(
+                _run_parameter_iteration(
+                    scenario=experiment.scenario,
+                    parameter_to_vary=experiment.parameter_to_vary,
+                    value=value,
+                )
+            )
+    return results
+
+
+def output_logs(
+    results: Dict[str, List[List[Tuple[List[Any], Dict[str, Any]]]]],
+    parameter_name: str,
+    experiment: Experiment,
+    result_folder: str = OUTPUT_DIR,
+    col_ordering: Optional[List[str]] = None,
+):
+    final_folder = pathlib.Path(result_folder) / experiment.name
+    os.makedirs(final_folder, exist_ok=True)
+
+    with open(final_folder / "run_config.json", "w") as f:
+        json.dump(experiment.to_dict(), f, indent=4, sort_keys=True)
+
+    for value, result_list in results.items():
+        file_path = final_folder / FILE_PATTERN.format(
+            param_name=parameter_name, param_value=value
+        )
+        with open(file_path, "w") as f:
+            csv_writer = csv.writer(f, delimiter=",")
+            # Keys should not change throughout, so just take the first
+            headers = (
+                list(result_list[0][0].keys()) if col_ordering is None else col_ordering
+            )
+
+            csv_writer.writerow(["Iteration"] + headers)
+            for i, res in enumerate(result_list):
+                for row in res:
+                    csv_writer.writerow([i] + [row[c] for c in headers])
