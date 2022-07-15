ae6a9b59c655d663d146cb7d914a036e3d8aae7b
Tom McLean <tom@vegaprotocol.io>
2022-07-15 14:00:47 +0100

    feat: Update latest changes

--- vega_sim/parameter_test/parameter/configs.py
+++ vega_sim/parameter_test/parameter/configs.py
@@ -50,21 +50,29 @@ TARGET_STAKE_SCALING_FACTOR_IDEAL_v2 = SingleParameterExperiment(
     parameter_to_vary="market.stake.target.scalingFactor",
     values=["0.5", "5", "50"],
     scenario=IdealMarketMakerV2(
-        initial_price=10,
+        market_decimal=3,
+        asset_decimal=5,
+        market_position_decimal=2,
+        initial_price=1123.11,
+        spread=0.002,
+        lp_commitamount=2000000,
         step_length_seconds=60,
         block_length_seconds=1,
-        buy_intensity=2,
-        sell_intensity=2,
-        kappa=1,
-        sigma=200,
-        num_steps=200,
+        buy_intensity=10,
+        sell_intensity=10,
+        q_upper=50,
+        q_lower=-50,
+        kappa=50,
+        sigma=0.5,
+        num_steps=288,
         state_extraction_fn=ideal_market_maker_single_data_extraction(
             additional_data_fns=[
+                tau_scaling_additional_data,
                 target_stake_additional_data,
             ]
         ),
     ),
-    runs_per_scenario=20,
+    runs_per_scenario=2,
     additional_parameters_to_set=[
         ("market.liquidity.targetstake.triggering.ratio", "1")
     ],
@@ -75,27 +83,72 @@ TAU_SCALING_FACTOR_IDEAL_v2 = SingleParameterExperiment(
     parameter_to_vary="market.liquidity.probabilityOfTrading.tau.scaling",
     values=["1", "10", "100"],
     scenario=IdealMarketMakerV2(
-        initial_price=10,
+        market_decimal=3,
+        asset_decimal=5,
+        market_position_decimal=2,
+        initial_price=1123.11,
+        spread=0.002,
+        lp_commitamount=2000000,
         step_length_seconds=60,
         block_length_seconds=1,
-        buy_intensity=2,
-        sell_intensity=2,
-        kappa=1,
-        sigma=200,
-        num_steps=200,
+        buy_intensity=10,
+        sell_intensity=10,
+        q_upper=50,
+        q_lower=-50,
+        kappa=50,
+        sigma=0.5,
+        num_steps=288,
         state_extraction_fn=ideal_market_maker_single_data_extraction(
-            additional_data_fns=[tau_scaling_additional_data]
+            additional_data_fns=[
+                tau_scaling_additional_data,
+                target_stake_additional_data,
+            ]
         ),
     ),
-    runs_per_scenario=3,
+    runs_per_scenario=2,
     additional_parameters_to_set=[
         ("market.liquidity.targetstake.triggering.ratio", "1")
     ],
 )
+
+BOND_PENALTY_FACTOR_IDEAL_v2 = SingleParameterExperiment(
+    name="BondPenalty_v2",
+    parameter_to_vary="market.liquidity.bondPenaltyParameter",
+    values=["0.5", "10", "50"],
+    scenario=IdealMarketMakerV2(
+        market_decimal=3,
+        asset_decimal=5,
+        market_position_decimal=2,
+        initial_price=1123.11,
+        spread=0.002,
+        lp_commitamount=2000000,
+        step_length_seconds=60,
+        block_length_seconds=1,
+        buy_intensity=10,
+        sell_intensity=10,
+        q_upper=50,
+        q_lower=-50,
+        kappa=50,
+        sigma=0.5,
+        num_steps=288,
+        state_extraction_fn=ideal_market_maker_single_data_extraction(
+            additional_data_fns=[
+                tau_scaling_additional_data,
+                target_stake_additional_data,
+            ]
+        ),
+    ),
+    runs_per_scenario=2,
+    additional_parameters_to_set=[
+        ("market.liquidity.targetstake.triggering.ratio", "1")
+    ],
+)
+
 
 CONFIGS = [
     TARGET_STAKE_SCALING_FACTOR_IDEAL,
     TAU_SCALING_FACTOR_IDEAL,
     TAU_SCALING_FACTOR_IDEAL_v2,
     TARGET_STAKE_SCALING_FACTOR_IDEAL_v2,
+    BOND_PENALTY_FACTOR_IDEAL_v2,
 ]
