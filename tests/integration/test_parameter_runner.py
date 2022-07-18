0e53da51e5b4a0672d6ff054b693a94d09b7cd83
Tom McLean <tom@vegaprotocol.io>
2022-07-18 10:57:10 +0100

    test: refactor tests

--- tests/integration/test_parameter_runner.py
+++ tests/integration/test_parameter_runner.py
@@ -1,3 +1,4 @@
+from tempfile import TemporaryDirectory
 import pytest
 
 import vega_sim.parameter_test.parameter.experiment as experiment
@@ -5,11 +6,16 @@ from vega_sim.parameter_test.parameter.configs import CONFIGS
 
 
 @pytest.mark.integration
-def test_parameter_runner():
-    config_map = {c.name: c for c in CONFIGS}
-    for key in config_map:
-        experiment_to_run = config_map[key]
+@pytest.mark.parametrize("experiment_to_run", CONFIGS)
+def test_parameter_runner(experiment_to_run: experiment.SingleParameterExperiment):
+    with TemporaryDirectory() as temp_dir:
+        experiment_to_run.runs_per_scenario = (
+            1  # Likely only need to test the one run per scenario
+        )
         results = experiment.run_single_parameter_experiment(experiment_to_run)
         experiment.output_logs(
-            results, experiment_to_run.parameter_to_vary, experiment=experiment_to_run
+            results,
+            experiment_to_run.parameter_to_vary,
+            experiment=experiment_to_run,
+            result_folder=temp_dir,
         )
