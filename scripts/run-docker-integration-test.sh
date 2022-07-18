0a400270ebb45f05ce9417968ec8ddc958d73f64
Tom McLean <tom@vegaprotocol.io>
2022-07-18 15:06:43 +0100

    feat: Logging in tests

--- scripts/run-docker-integration-test.sh
+++ scripts/run-docker-integration-test.sh
@@ -1,7 +1,7 @@
 docker run \
     --platform linux/amd64 \
     --name vega_test \
-    vega_sim_test:latest pytest -s -v -m integration
+    vega_sim_test:latest pytest -s -v -m integration --log-cli-level INFO
 docker_status=$?
 docker cp vega_test:/tmp ./test_logs/
 docker rm vega_test
