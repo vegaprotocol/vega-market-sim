0a3628bd30753b150aa127f14611e0f15b0cae21
Galen Cao <galenc0084@gmail.com>
2022-07-19 18:59:54 +0100

    Update configs

--- vega_sim/parameter_test/parameter/configs.py
+++ vega_sim/parameter_test/parameter/configs.py
@@ -4,6 +4,7 @@ from vega_sim.parameter_test.parameter.loggers import (
     target_stake_additional_data,
     v1_ideal_mm_additional_data,
     tau_scaling_additional_data,
+    limit_order_book,
 )
 from vega_sim.scenario.registry import IdealMarketMaker, IdealMarketMakerV2
 
@@ -54,7 +55,7 @@ TARGET_STAKE_SCALING_FACTOR_IDEAL_v2 = SingleParameterExperiment(
         asset_decimal=5,
         market_position_decimal=2,
         initial_price=1123.11,
-        spread=0.002,
+        spread=4,
         lp_commitamount=1000000,
         initial_asset_mint=1e8,
         step_length_seconds=60,
@@ -66,10 +67,13 @@ TARGET_STAKE_SCALING_FACTOR_IDEAL_v2 = SingleParameterExperiment(
         kappa=50,
         sigma=0.5,
         num_steps=288,
+        backgroundmarket_tick_spacing=0.002,
+        backgroundmarket_number_levels_per_side=20,
         state_extraction_fn=ideal_market_maker_single_data_extraction(
             additional_data_fns=[
                 tau_scaling_additional_data,
                 target_stake_additional_data,
+                limit_order_book,
             ]
         ),
     ),
@@ -88,7 +92,7 @@ TAU_SCALING_FACTOR_IDEAL_v2 = SingleParameterExperiment(
         asset_decimal=5,
         market_position_decimal=2,
         initial_price=1123.11,
-        spread=0.002,
+        spread=4,
         lp_commitamount=1000000,
         initial_asset_mint=1e8,
         step_length_seconds=60,
@@ -100,14 +104,17 @@ TAU_SCALING_FACTOR_IDEAL_v2 = SingleParameterExperiment(
         kappa=50,
         sigma=0.5,
         num_steps=288,
+        backgroundmarket_tick_spacing=0.002,
+        backgroundmarket_number_levels_per_side=25,
         state_extraction_fn=ideal_market_maker_single_data_extraction(
             additional_data_fns=[
                 tau_scaling_additional_data,
                 target_stake_additional_data,
+                limit_order_book,
             ]
         ),
     ),
-    runs_per_scenario=1,
+    runs_per_scenario=2,
     additional_parameters_to_set=[
         ("market.liquidity.targetstake.triggering.ratio", "1")
     ],
