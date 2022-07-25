70ac372c1502412fbcfe118c91032f432f30997d
Tom McLean <tom@vegaprotocol.io>
2022-07-25 17:29:24 +0100

    test: Mark new test as integratin

--- tests/integration/test_multiple_instantiation.py
+++ tests/integration/test_multiple_instantiation.py
@@ -1,8 +1,10 @@
+import pytest
 from vega_sim.null_service import VegaServiceNull
 
 
 # Simply testing that multiple Vegas start
 # in the same process ok
+@pytest.mark.integration
 def test_multiple_instantiation():
     with VegaServiceNull(
         run_with_console=False,
