2876f68ebdbb86164029024ca501e1c4a270d5fc
Tom McLean <tom@vegaprotocol.io>
2022-08-17 17:50:22 +0100

    feat: Update tests

--- scripts/run-docker-example-notebook-test.sh
+++ scripts/run-docker-example-notebook-test.sh
@@ -1,5 +1,3 @@
 docker run --rm \
     --platform linux/amd64 \
-    # --mount type=bind,source=/Users/tom/Code/vega-market-sim/examples,target=/vega_market_sim/examples \
-    # --mount type=bind,source=/tmp,target=/tmp \
-    vega_sim_test:latest pytest --log-cli-level INFO --nbmake examples/notebooks/Settlement.ipynb
+    vega_sim_test:latest pytest --log-cli-level INFO --nbmake examples/notebooks
