00ff1ccb803999c695988d37a12d4f963000a590
Tom McLean <tom@vegaprotocol.io>
2022-08-19 16:53:43 +0100

    test: Update test naming

--- scripts/run-docker-integration-test.sh
+++ scripts/run-docker-integration-test.sh
@@ -4,8 +4,6 @@ docker rm ${container_name} || true
 docker run \
     --platform linux/amd64 \
     --name ${container_name} \
-    --mount type=bind,source=/tmp,target=/tmp \
-    --mount type=bind,source=/Users/tom/Code/vega-market-sim/tests,target=/vega_market_sim/tests \
     vega_sim_test:latest pytest -s -v -m integration --log-cli-level INFO tests/integration/test_openoracle_settlement.py
 docker_status=$?
 docker cp ${container_name}:/tmp ./test_logs/
