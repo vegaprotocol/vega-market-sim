8b624403f9b8e7c0044c9dcf3714b5958fab8aa3
Charlie <charles.ae.dummett@outlook.com>
2022-09-01 10:39:29 +0100

    feat: update comprehensive_market scenario

--- vega_sim/scenario/comprehensive_market/agents.py
+++ vega_sim/scenario/comprehensive_market/agents.py
@@ -2,28 +2,20 @@ from collections import namedtuple
 
 WalletConfig = namedtuple("WalletConfig", ["name", "passphrase"])
 
-# Set up wallets for parties submitting / amending liquidity provisions
+# Set up wallets for parties proposing market
 MM_WALLET = WalletConfig("mm", "pin")
 
-BG_WALLET = WalletConfig("bg", "pin")
+# Set up wallets for parties submitting market orders
+lp_agents = 5
+LP_WALLETS = [WalletConfig("lp_trader_" + str(i), "pass") for i in range(lp_agents)]
 
 # Set up wallets for parties submitting market orders
-MO_WALLETS = (
-    WalletConfig("mo_trader_a", "pass"),
-    WalletConfig("mo_trader_b", "pass"),
-    WalletConfig("mo_trader_c", "pass"),
-    WalletConfig("mo_trader_d", "pass"),
-    WalletConfig("mo_trader_e", "pass"),
-)
+mo_agents = 5
+MO_WALLETS = [WalletConfig("mo_trader_" + str(i), "pass") for i in range(mo_agents)]
 
 # Set up wallets for parties submitting / cancelling limit orders
-LO_WALLETS = (
-    WalletConfig("lo_trader_a", "pass"),
-    WalletConfig("lo_trader_b", "pass"),
-    WalletConfig("lo_trader_c", "pass"),
-    WalletConfig("lo_trader_d", "pass"),
-    WalletConfig("lo_trader_e", "pass"),
-)
+lo_agents = 20
+LO_WALLETS = [WalletConfig("lo_trader_" + str(i), "pass") for i in range(lo_agents)]
 
 # Set up wallets for parties to force auction pass
 AUCTION1_WALLET = WalletConfig("AUCTION1", "AUCTION1pass")
--- vega_sim/scenario/comprehensive_market/scenario.py
+++ vega_sim/scenario/comprehensive_market/scenario.py
@@ -12,17 +12,16 @@ from vega_sim.environment.environment import MarketEnvironmentWithState
 from vega_sim.null_service import VegaServiceNull
 from vega_sim.scenario.comprehensive_market.agents import (
     MM_WALLET,
-    BG_WALLET,
+    LP_WALLETS,
     MO_WALLETS,
     LO_WALLETS,
     AUCTION1_WALLET,
     AUCTION2_WALLET,
     TERMINATE_WALLET,
 )
-
-from vega_sim.scenario.ideal_market_maker_v2.agents import OptimalMarketMaker
 from vega_sim.scenario.common.agents import (
-    BackgroundMarket,
+    MarketManager,
+    LiquidityProvider,
     MarketOrderTrader,
     LimitOrderTrader,
     OpenAuctionPass,
@@ -42,20 +41,11 @@ class ComprehensiveMarket(Scenario):
         initial_asset_mint: float = 1000000,
         initial_price: Optional[float] = None,
         sigma: float = 1,
-        kappa: float = 1,
-        q_upper: int = 20,
-        q_lower: int = -20,
-        alpha: float = 10**-4,
-        phi: float = 5 * 10**-6,
         lp_commitamount: float = 200000,
         spread: int = 2,
         block_size: int = 1,
         block_length_seconds: int = 1,
         state_extraction_freq: int = 1,
-        buy_intensity: float = 5,
-        sell_intensity: float = 5,
-        backgroundmarket_tick_spacing: float = 0.002,
-        backgroundmarket_number_levels_per_side: int = 20,
         step_length_seconds: int = 1,
         opening_auction_trade_amount: float = 1,
         state_extraction_fn: Optional[
@@ -63,10 +53,18 @@ class ComprehensiveMarket(Scenario):
         ] = None,
         pause_every_n_steps: Optional[int] = None,
         price_process_fn: Optional[Callable[[None], List[float]]] = None,
-        market_order_trader_base_order_size: float = 1,
+        market_order_trader_order_intensity: int  = 10,
+        market_order_trader_order_size: float = 0.1,
+        settle_at_end: bool = True,
+        limit_order_trader_quantity: int = 5,
+        limit_order_trader_submit_bias: float = 0.5,
+        limit_order_trader_cancel_bias: float = 0.5,
+        limit_order_trader_order_intensity: int = 10,
+        limit_order_trader_order_size: float = 0.1,
         limit_order_trader_mean: float = -5,
         limit_order_trader_sigma: float = 0.5,
-        settle_at_end: bool = True,
+        limit_order_trader_duration: int = 300,
+        limit_order_trader_time_in_force_opts: Optional[dict] = None,
     ):
         self.num_steps = num_steps
         self.dt = dt
@@ -75,34 +73,37 @@ class ComprehensiveMarket(Scenario):
         self.market_position_decimal = market_position_decimal
         self.initial_price = initial_price
         self.sigma = sigma
-        self.kappa = kappa
-        self.q_upper = q_upper
-        self.q_lower = q_lower
-        self.alpha = alpha
-        self.phi = phi
         self.spread = spread / 10**self.market_decimal
         self.block_size = block_size
         self.block_length_seconds = block_length_seconds
         self.state_extraction_freq = state_extraction_freq
         self.step_length_seconds = step_length_seconds
         self.state_extraction_fn = state_extraction_fn
-        self.buy_intensity = buy_intensity
-        self.sell_intensity = sell_intensity
         self.pause_every_n_steps = pause_every_n_steps
         self.lp_commitamount = lp_commitamount
         self.initial_asset_mint = initial_asset_mint
-        self.backgroundmarket_tick_spacing = backgroundmarket_tick_spacing
-        self.backgroundmarket_number_levels_per_side = (
-            backgroundmarket_number_levels_per_side
-        )
         self.market_name = "ETH:USD" if market_name is None else market_name
         self.asset_name = "tDAI" if asset_name is None else asset_name
         self.price_process_fn = price_process_fn
         self.opening_auction_trade_amount = opening_auction_trade_amount
-        self.market_order_trader_base_order_size = market_order_trader_base_order_size
+        self.settle_at_end = settle_at_end
+
+        # MarketOrderTraderOptions
+        self.market_order_trader_order_intensity = market_order_trader_order_intensity
+        self.market_order_trader_order_size = market_order_trader_order_size
+
+        # LimitOrderTrader Options
+        self.limit_order_trader_quantity = limit_order_trader_quantity
+        self.limit_order_trader_submit_bias = limit_order_trader_submit_bias
+        self.limit_order_trader_cancel_bias = limit_order_trader_cancel_bias
+        self.limit_order_trader_order_intensity = limit_order_trader_order_intensity
+        self.limit_order_trader_order_size = limit_order_trader_order_size
         self.limit_order_trader_mean = limit_order_trader_mean
         self.limit_order_trader_sigma = limit_order_trader_sigma
-        self.settle_at_end = settle_at_end
+        self.limit_order_trader_duration = limit_order_trader_duration
+        self.limit_order_trader_time_in_force_opts = (
+            limit_order_trader_time_in_force_opts
+        )
 
     def _generate_price_process(
         self,
@@ -134,21 +135,11 @@ class ComprehensiveMarket(Scenario):
             else self._generate_price_process(random_state=random_state)
         )
 
-        market_maker = OptimalMarketMaker(
+        mm_agent = MarketManager(
             wallet_name=MM_WALLET.name,
             wallet_pass=MM_WALLET.passphrase,
             terminate_wallet_name=TERMINATE_WALLET.name,
             terminate_wallet_pass=TERMINATE_WALLET.passphrase,
-            initial_asset_mint=self.initial_asset_mint,
-            price_process=price_process,
-            spread=self.spread,
-            num_steps=self.num_steps,
-            market_order_arrival_rate=self.buy_intensity,
-            kappa=self.kappa,
-            inventory_upper_boundary=self.q_upper,
-            inventory_lower_boundary=self.q_lower,
-            terminal_penalty_parameter=self.alpha,
-            running_penalty_parameter=self.phi,
             asset_decimal=self.asset_decimal,
             market_decimal=self.market_decimal,
             market_position_decimal=self.market_position_decimal,
@@ -159,7 +150,22 @@ class ComprehensiveMarket(Scenario):
             settlement_price=price_process[-1] if self.settle_at_end else None,
         )
 
-        mo_traders = [
+        lp_agents = [
+            LiquidityProvider(
+                wallet_name=LP_WALLETS[i].name,
+                wallet_pass=LP_WALLETS[i].passphrase,
+                initial_asset_mint=self.initial_asset_mint,
+                market_name=market_name,
+                asset_name=asset_name,
+                tag=str(tag),
+                commitment_amount=self.lp_commitamount,
+                fee=0.001,
+                offset=self.spread * (i + 1),
+            )
+            for i in range(len(LP_WALLETS))
+        ]
+
+        mo_agents = [
             MarketOrderTrader(
                 wallet_name=MO_WALLETS[i].name,
                 wallet_pass=MO_WALLETS[i].passphrase,
@@ -167,30 +173,36 @@ class ComprehensiveMarket(Scenario):
                 market_name=market_name,
                 asset_name=asset_name,
                 tag=str(tag),
-                buy_intensity=self.buy_intensity,
-                sell_intensity=self.sell_intensity,
+                buy_intensity=self.market_order_trader_order_intensity,
+                sell_intensity=self.market_order_trader_order_intensity,
+                base_order_size=self.market_order_trader_order_size,
                 random_state=random_state,
-                base_order_size=self.market_order_trader_base_order_size,
+                
             )
             for i in range(len(MO_WALLETS))
         ]
 
-        lo_traders = [
+        lo_agents = [
             LimitOrderTrader(
                 wallet_name=LO_WALLETS[i].name,
                 wallet_pass=LO_WALLETS[i].passphrase,
-                spread=self.spread,
                 initial_asset_mint=self.initial_asset_mint,
                 market_name=market_name,
                 asset_name=asset_name,
                 tag=str(tag),
-                buy_intensity=self.buy_intensity,
-                sell_intensity=self.sell_intensity,
-                random_state=random_state,
-                submit_bias=0.8,
-                cancel_bias=0.8,
+                spread=self.spread,
+                price_process=price_process,
+                buy_intensity=self.limit_order_trader_order_intensity,
+                sell_intensity=self.limit_order_trader_order_intensity,
+                buy_volume=self.limit_order_trader_order_size,
+                sell_volume=self.limit_order_trader_order_size,
+                submit_bias=self.limit_order_trader_submit_bias,
+                cancel_bias=self.limit_order_trader_cancel_bias,
+                duration=self.limit_order_trader_duration,
+                time_in_force_opts=self.limit_order_trader_time_in_force_opts,
                 mean=self.limit_order_trader_mean,
                 sigma=self.limit_order_trader_sigma,
+                random_state=random_state,
             )
             for i in range(len(LO_WALLETS))
         ]
@@ -223,30 +235,15 @@ class ComprehensiveMarket(Scenario):
             tag=str(tag),
         )
 
-        background_market = BackgroundMarket(
-            wallet_name=BG_WALLET.name,
-            wallet_pass=BG_WALLET.passphrase,
-            market_name=market_name,
-            asset_name=asset_name,
-            initial_asset_mint=self.initial_asset_mint,
-            price_process=price_process,
-            spread=self.spread,
-            tick_spacing=self.backgroundmarket_tick_spacing,
-            order_distribution_kappa=self.kappa,
-            num_levels_per_side=self.backgroundmarket_number_levels_per_side,
-            tag=str(tag),
-            position_decimals=self.market_position_decimal,
-        )
-
         env = MarketEnvironmentWithState(
             agents=[
-                market_maker,
+                mm_agent,
                 auctionpass1,
                 auctionpass2,
-                background_market,
             ]
-            + mo_traders
-            + lo_traders,
+            + lp_agents
+            + mo_agents
+            + lo_agents,
             n_steps=self.num_steps,
             transactions_per_block=self.block_size,
             vega_service=vega,
