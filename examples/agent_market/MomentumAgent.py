f9d2aeeb47da0e66a563c94ce6bdee4ffe7110b1
Galen-Cao <galenc0084@gmail.com>
2022-09-12 10:34:52 +0100

    fix: make black run

--- examples/agent_market/MomentumAgent.py
+++ examples/agent_market/MomentumAgent.py
@@ -49,9 +49,9 @@ scenario = ComprehensiveMarket(
     num_mo_agents=0,
     momentum_trader_order_intensity=100,
     momentum_trader_order_size=0.01,
-    momentum_trader_strategies=['RSI'],
-    momentum_trader_strategy_args=[{'timeperiod': 7}],
-    momentum_trader_indicator_thresholds=[(70,30)], 
+    momentum_trader_strategies=["RSI"],
+    momentum_trader_strategy_args=[{"timeperiod": 7}],
+    momentum_trader_indicator_thresholds=[(70, 30)],
     state_extraction_fn=momentum_trader_data_extraction,
 )
 
