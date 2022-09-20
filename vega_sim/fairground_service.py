0e6d55df1bfcef6f872bb3aa7a283e98d49de5bf
Charlie <charles.ae.dummett@outlook.com>
2022-09-20 12:26:05 +0100

    feat: fairground service updated

--- vega_sim/fairground_service.py
+++ vega_sim/fairground_service.py
@@ -1,8 +1,9 @@
 from typing import Dict, List, Optional, Set
 
-import requests
-import toml
-from urllib3.exceptions import MaxRetryError
+import time
+import subprocess
+
+from os import path
 
 from vega_sim import vega_bin_path, vega_home_path
 from vega_sim.service import VegaService
@@ -11,17 +12,32 @@ from vega_sim.wallet.slim_wallet import SlimWallet
 from vega_sim.wallet.vega_wallet import VegaWallet
 
 
+def start_wallet_service():
+
+    wallet_args = [
+        "vega",
+        "wallet",
+        "service",
+        "run",
+        "--network",
+        "fairground",
+        "--automatic-consent",
+    ]
+
+    process = subprocess.Popen(args=wallet_args)
+
+    return process
+
+
 class VegaServiceFairground(VegaService):
     def __init__(
         self,
-        use_full_vega_wallet: bool = True,
+        warn_on_raw_data_access: bool = False,
     ):
         super().__init__(
-            can_control_time=False,
+            can_control_time=False, warn_on_raw_data_access=warn_on_raw_data_access
         )
-
         self._wallet = None
-        self._use_full_vega_wallet = use_full_vega_wallet
 
     def __enter__(self):
         self.start()
@@ -30,25 +46,22 @@ class VegaServiceFairground(VegaService):
     def __exit__(self, type, value, traceback):
         self.stop()
 
+    def start(self):
+        self.process = start_wallet_service()
+
+    def stop(self):
+        self.process.kill()
+
     @property
     def data_node_grpc_url(self) -> str:
-        return "api.n07.testnet.vega.xyz:3007"
+        return "api.n09.testnet.vega.xyz:3007"
 
     @property
     def wallet_url(self) -> str:
         return "http://127.0.0.1:1789"
 
-
     @property
     def wallet(self) -> Wallet:
         if self._wallet is None:
-            if self._use_full_vega_wallet:
-                self._wallet = VegaWallet(self.wallet_url)
-            else:
-                self._wallet = SlimWallet(
-                    self.core_client,
-                    full_wallet=VegaWallet(self.wallet_url)
-                    if self.run_with_console
-                    else None,
-                )
+            self._wallet = VegaWallet(self.wallet_url)
         return self._wallet
