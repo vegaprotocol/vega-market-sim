071ffdad5efe2466807c4dddf9c763654ff6803b
Tom McLean <tom@vegaprotocol.io>
2022-09-28 16:59:10 +0100

    test: Try changes to make tests stable

--- vega_sim/replay/replay.py
+++ vega_sim/replay/replay.py
@@ -23,6 +23,7 @@ def replay_run_context(
             transactions_per_block=tx_per_block,
             warn_on_raw_data_access=False,
         ) as vega:
+            time.sleep(0.005)
             vega.wait_for_total_catchup()
             next_tx_type = tx_history.read(TRANSACTION_LEN_BYTES)
             while next_tx_type:
