798989c16c27e4070cec67a62ee8c97fbccaa76f
Tom McLean <tom@vegaprotocol.io>
2022-10-11 18:05:18 -0500

    feat: Updating agent layout

--- examples/nullchain.py
+++ examples/nullchain.py
@@ -28,8 +28,8 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     with VegaServiceNull(
-        run_with_console=True,
-        launch_graphql=True,
+        run_with_console=False,
+        launch_graphql=False,
         start_order_feed=True,
         retain_log_files=True,
         use_full_vega_wallet=False,
