fix: correct buy sell calculation

vega_sim/scenario/common/agents.py
@@ -239,10 +239,10 @@ class PriceSensitiveMarketOrderTrader(StateAgentWithWallet):
         best_bid, best_ask = self.vega.best_prices(self.market_id)
 
         will_buy = self.random_state.rand() < np.exp(
-            -1 * self.probability_decay * abs(best_bid - self.curr_price)
+            -1 * self.probability_decay * abs(best_ask - self.curr_price)
         )
         will_sell = self.random_state.rand() < np.exp(
-            -1 * self.probability_decay * abs(best_ask - self.curr_price)
+            -1 * self.probability_decay * abs(best_bid - self.curr_price)
         )
 
         if buy_first and will_buy: