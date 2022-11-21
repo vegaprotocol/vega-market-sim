6f426523b69588b55035a7dd302bc5fde05cd515
Tom McLean <tom@vegaprotocol.io>
2022-11-21 11:53:20 +0000

    feat: Update workflows

--- vega_sim/__init__.py
+++ vega_sim/__init__.py
@@ -1,6 +1,8 @@
+from importlib import metadata
 from pathlib import Path
 
-__version__ = "0.62.2"
+
+__version__ = metadata.version(__package__)
 
 vega_home_path = Path(__file__).parent / "vegahome"
 vega_bin_path = Path(__file__).parent / "bin"
