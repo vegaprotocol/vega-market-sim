75d952d9cca6a2cb1d750cda699aac4e385e6853
Tom McLean <tom@vegaprotocol.io>
2022-11-23 18:55:59 +0000

    feat: Add profiling to run

--- scripts/run-docker-integration-test.sh
+++ scripts/run-docker-integration-test.sh
@@ -4,13 +4,17 @@ set -e
 
 WORK_DIR="$(realpath "$(dirname "$0")/..")"
 RESULT_DIR="${WORK_DIR}/test_logs/$(date '+%F_%H%M%S')-integration"
+PROF_DIR="${WORK_DIR}/prof/$(date '+%F_%H%M%S')-integration"
 mkdir -p "${RESULT_DIR}"
+mkdir -p "${PROF_DIR}"
 
 docker run \
     --platform linux/amd64 \
+    --name integration_test
     -v "${RESULT_DIR}:/tmp" \
+    -v "${PROF_DIR}:/vega_market_sim/prof" \
     vega_sim_test:latest \
-        pytest -s -v \
+        pytest -s -v --profile \
             -m integration \
             --junitxml /tmp/integration-test-results.xml \
             --log-cli-level INFO 
