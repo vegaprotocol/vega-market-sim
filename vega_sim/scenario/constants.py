d800ae04e1b238f3b977064f88cba8aec9bfc711
Charlie <charles.ae.dummett@outlook.com>
2022-11-25 11:53:39 +0000

    fix: update STAGNET3 value

--- vega_sim/scenario/constants.py
+++ vega_sim/scenario/constants.py
@@ -5,5 +5,5 @@ class Network(Enum):
     NULLCHAIN = "nullchain"
     STAGNET1 = "stagnet1"
     STAGNET2 = "vegawallet-stagnet2"
-    STAGNET3 = "stagnet3"
+    STAGNET3 = "vegawallet-stagnet3"
     FAIRGROUND = "fairground"
