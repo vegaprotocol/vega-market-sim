48fc746b35a9e93684a5e0756ccbdfa0e7dd155f
Tom McLean <tom@vegaprotocol.io>
2022-11-25 11:51:39 +0000

    feat: Explicitly use buildx

--- scripts/build-docker-learning.sh
+++ scripts/build-docker-learning.sh
@@ -1,1 +1,1 @@
-docker build --platform linux/amd64 --tag=vega_sim_learning:latest -t vegasim_learning .
+docker buildx build --platform linux/amd64 --tag=vega_sim_learning:latest -t vegasim_learning .
