8189d6e99acd313eae3e493339c5869f147496f5
Charlie <charles.ae.dummett@outlook.com>
2022-12-20 12:04:36 +0000

    refactor: condense wallets

--- vega_sim/scenario/hedged_market_maker/agents.py
+++ vega_sim/scenario/hedged_market_maker/agents.py
@@ -2,16 +2,16 @@ from collections import namedtuple
 
 PartyConfig = namedtuple("PartyConfig", ["wallet_name", "wallet_pass", "key_name"])
 
-MARKET_CREATOR = PartyConfig("aux_parties", "pass", "Market Creator")
-MARKET_SETTLER = PartyConfig("aux_parties", "pass", "Market Settler")
+MARKET_CREATOR = PartyConfig("vega", "pass", "Market Creator")
+MARKET_SETTLER = PartyConfig("vega", "pass", "Market Settler")
 
-AUCTION_PASS_BID = PartyConfig("aux_parties", "pass", "Auction Pass (Bid)")
-AUCTION_PASS_ASK = PartyConfig("aux_parties", "pass", "Auction Pass (Ask)")
+AUCTION_PASS_BID = PartyConfig("vega", "pass", "Auction Pass (Bid)")
+AUCTION_PASS_ASK = PartyConfig("vega", "pass", "Auction Pass (Ask)")
 
-INT_MARKET_MAKER_KEY_A = PartyConfig("market_maker", "pass", "Key A (Internal)")
-INT_MARKET_MAKER_KEY_B = PartyConfig("market_maker", "pass", "Key B (External)")
-INT_INFORMED_TRADER = PartyConfig("aux_parties", "pass", "Informed Trader")
-INT_RANDOM_TRADER = PartyConfig("aux_parties", "pass", "Random Trader (Internal)")
+INT_MARKET_MAKER_KEY_A = PartyConfig("vega", "pass", "Key A (Internal)")
+INT_MARKET_MAKER_KEY_B = PartyConfig("vega", "pass", "Key B (External)")
+INT_INFORMED_TRADER = PartyConfig("vega", "pass", "Informed Trader")
+INT_RANDOM_TRADER = PartyConfig("vega", "pass", "Random Trader (Internal)")
 
-EXT_MARKET_MAKER = PartyConfig("aux_parties", "pass", "External Market Maker")
-EXT_RANDOM_TRADER = PartyConfig("aux_parties", "pass", "Random Trader (External)")
+EXT_MARKET_MAKER = PartyConfig("vega", "pass", "External Market Maker")
+EXT_RANDOM_TRADER = PartyConfig("vega", "pass", "Random Trader (External)")
