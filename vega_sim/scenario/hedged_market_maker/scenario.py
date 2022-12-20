cb53aee15e646a7ef34f58ecafe9adadd0ef3c3c
Charlie <charles.ae.dummett@outlook.com>
2022-12-20 12:02:49 +0000

    refactor: add documentation

--- vega_sim/scenario/hedged_market_maker/scenario.py
+++ vega_sim/scenario/hedged_market_maker/scenario.py
@@ -1,8 +1,53 @@
+"""scenario.py
+
+Module contains the HedgedMarket Scenario class which can be used to run a simulation of
+a hedged market maker who makes a market on an internal market and hedges its position
+on a highly liquid external market. The hedged market maker uses separate keys for each
+market to represent the accounts on separate exchanges and uses transfers to balance
+each accounts leverage. Transfer delays can be configured to represent the withdrawal
+delays on the internal and external markets.
+
+The scenario uses an InformedTrader agent, who has knowledge of all future prices, to
+modify whether the hedged market maker makes a profit on the internal or external
+market. This is done by modifying the accuracy of the InformedTrader (0.0 causes the
+hedged market maker to profit in the internal market and 1.0 causes the hedged market 
+maker to profit in the external market).
+
+Agents:
+
+    int_market_manager (MarketManager):
+        Proposes and settles the internal market.
+    ext_market_manager (MarketManager):
+        Proposes and settles the external market.
+    int_market_maker (HedgedMarketMaker):
+        Makes the market on the internal market.
+    ext_market_maker (ExponentialShapedMarketMaker):
+        Makes the market on the external market.
+    int_auction_pass_bid (OpenAuctionPass):
+        Provides a best-bid to exit the auction on the internal market.
+    int_auction_pass_bid (OpenAuctionPass):
+        Provides a best-ask to exit the auction on the internal market.
+    ext_auction_pass_bid (OpenAuctionPass):
+        Provides a best-bid to exit the auction on the internal market.
+    ext_auction_pass_bid (OpenAuctionPass):
+        Provides a best-ask to exit the auction on the internal market.
+    int_random_trader (MarketOrderTrader):
+        Provides a small volume trade at each step on the internal market.
+    ext_random_trader (MarketOrderTrader):
+        Provides a small volume trade at each step on the external market.
+    int_informed_trader (InformedTrader):
+        Places informed orders at each step. Can be configured to cause the hedged
+        market maker to have a +ve or -ve PnL in either market.
+
+Examples:
+
+    $ python -m vega_sim.scenario.adhoc -s hedged_market --console --pause
+
+"""
+
 import numpy as np
 from typing import Any, Callable, List, Optional
 from vega_sim.environment.agent import Agent
-
-from vega_sim.api.market import MarketConfig
 
 from vega_sim.scenario.scenario import Scenario
 from vega_sim.scenario.common.utils.price_process import random_walk
@@ -31,6 +76,82 @@ from vega_sim.scenario.common.agents import (
 
 
 class HedgedMarket(Scenario):
+    """Class defining a HedgedMarket scenario,
+
+    Scenario simulates a hedged market maker who makes a market on an internal market
+    and hedges their position on an external market. An informed trader can exploit
+    knowledge of future prices to make profitable trades with the hedged market maker.
+    The accuracy of this informed trader is configurable.
+
+    Args:
+        num_steps (int, optional):
+            Number of simulation steps to run. Defaults to 290.
+        step_length_seconds (int, optional):
+            Length of each step in seconds. Defaults to 60.
+        block_length_seconds (int, optional):
+            Length of each block in seconds. Defaults to 1.
+        price_process_fn (Optional[Callable[[None], List[float]]], optional):
+            Function used to generate a price process. Defaults to None.
+        initial_price (Optional[float], optional):
+            Initial price for RW if no price_process_fn specified. Defaults to None.
+        asset_name (str, optional):
+            Name of asset to use in simulation. Defaults to "ETH".
+        asset_adp (int, optional):
+            Number of decimal places for asset. Defaults to 5.
+        int_name (str, optional):
+            Name of the internal market. Defaults to "ETHUSD (Internal Market)".
+        int_mdp (int, optional):
+            Number of decimal places for price on the internal market. Defaults to 3.
+        int_pdp (int, optional):
+            Number of decimal places for position on the internal market. Defaults to 3.
+        int_lock (float, optional):
+            Internal market withdrawal delay length in seconds. Defaults to 24*60*60.
+        ext_name (str, optional):
+            Name of the external market. Defaults to "ETHUSD (External Market)".
+        ext_mdp (int, optional):
+            Number of decimal places for price on the external market. Defaults to 3.
+        ext_pdp (int, optional):
+            Number of decimal places for position on the external market. Defaults to 3.
+        ext_lock (float, optional):
+            External market withdrawal delay length in seconds. Defaults to 5*60.
+        int_mm_int_key_mint (float, optional):
+            Amount of asset to initially mint to the internal key. Defaults to 2e5.
+        int_mm_ext_key_mint (float, optional):
+            Amount of asset to initially mint to the external key. Defaults to 1e5.
+        int_mm_commitment_amount (float, optional):
+            Amount of asset to initially commit as liquidity to the internal market.
+            Defaults to 1e5.
+        int_mm_profit_margin (float, optional):
+            Desired profit margin to make on each trade on the internal market.
+            Defaults to 0.0000.
+        ext_mm_num_levels (int, optional):
+            Number of price levels on the external market. Defaults to 1e10.
+        ext_mm_tick_spacing (float, optional):
+            Spacing of price levels on the external market. Defaults to 0.1.
+        ext_mm_kappa (float, optional):
+            Order kappa on the external market. Defaults to 1.
+        ext_mm_market_kappa (float, optional):
+            Estimated market kappa on the external market. Defaults to 10.
+        int_it_proportion (float, optional):
+            Proportion of profitable orders taken by the informed trader at each step.
+            Defaults to 0.1.
+        int_it_accuracy (float, optional):
+            Accuracy of informed trader's speculation at each step.
+            Defaults to 1.0.
+        int_it_lookahead (int, optional):
+            Number of steps for informed trader to look ahead. Defaults to 30.
+        random_agent_ordering (bool, optional):
+            Whether to randomly order each agents step action. Defaults to False.
+        transactions_per_block (int, optional):
+            Number of transactions allowed per block. Defaults to 1000.
+        state_extraction_fn (Optional[ Callable[[VegaServiceNull, List[Agent]], Any] ], optional):
+            Defines which states to extract and record. Defaults to None.
+        state_extraction_freq (int, optional):
+            How often to run the state_extraction_fn. Defaults to 1.
+        pause_every_n_steps (Optional[int], optional):
+            Number of steps to simulate before pausing the simulation. Defaults to None.
+    """
+    
     def __init__(
         self,
         num_steps: int = 290,
@@ -52,7 +173,6 @@ class HedgedMarket(Scenario):
         int_mm_ext_key_mint: float = 1e10,
         int_mm_commitment_amount: float = 1e5,
         int_mm_profit_margin: float = 0.0000,
-        int_mm_max_leverage: float = 10.0,
         ext_mm_num_levels: int = 1e10,
         ext_mm_tick_spacing: float = 0.1,
         ext_mm_kappa: float = 1,
@@ -102,7 +222,6 @@ class HedgedMarket(Scenario):
         self.int_mm_ext_key_mint = int_mm_ext_key_mint
         self.int_mm_commitment_amount = int_mm_commitment_amount
         self.int_mm_profit_margin = int_mm_profit_margin
-        self.int_mm_max_leverage = int_mm_max_leverage
 
         # External market maker properties
         self.ext_mm_num_levels = ext_mm_num_levels
