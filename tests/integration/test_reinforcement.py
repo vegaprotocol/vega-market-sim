5c8ab472f29686894748353c208d47bf12dfd369
Tom McLean <tom@vegaprotocol.io>
2022-12-21 17:14:18 +0000

    feat: Fix up imports

--- tests/integration/test_reinforcement.py
+++ tests/integration/test_reinforcement.py
@@ -1,9 +1,9 @@
 import pytest
 
-import vega_sim.reinforcement.run_rl_agent as rl
-
 
 @pytest.mark.integration
 def test_rl_run():
     # Simply testing that it doesn't error
+    import vega_sim.reinforcement.run_rl_agent as rl
+
     rl._run(1)
