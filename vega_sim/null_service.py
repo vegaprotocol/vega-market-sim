50642ad4c0fc98ad63f7465453573acbd9b6e445
Charlie <99198652+cdummett@users.noreply.github.com>
2023-02-13 14:18:11 +0000

    fix: no wallet for console (#334)

--- vega_sim/null_service.py
+++ vega_sim/null_service.py
@@ -588,9 +588,7 @@ class VegaServiceNull(VegaService):
             else:
                 self._wallet = SlimWallet(
                     self.core_client,
-                    full_wallet=VegaWallet(self.wallet_url)
-                    if self.run_with_console
-                    else None,
+                    full_wallet=None,
                     log_dir=self.log_dir,
                 )
         return self._wallet
@@ -644,7 +642,7 @@ class VegaServiceNull(VegaService):
                 "port_config": port_config,
                 "transactions_per_block": self.transactions_per_block,
                 "block_duration": f"{int(self.seconds_per_block)}s",
-                "run_wallet": self._use_full_vega_wallet or self.run_with_console,
+                "run_wallet": self._use_full_vega_wallet,
                 "retain_log_files": self.retain_log_files,
                 "log_dir": self.log_dir,
                 "store_transactions": self.store_transactions,
@@ -670,7 +668,7 @@ class VegaServiceNull(VegaService):
                     requests.get(
                         f"http://localhost:{self.vega_node_rest_port}/blockchain/height"
                     ).raise_for_status()
-                    if self.run_with_console or self._use_full_vega_wallet:
+                    if self._use_full_vega_wallet:
                         requests.get(
                             f"http://localhost:{self.wallet_port}/api/v1/status"
                         ).raise_for_status()
