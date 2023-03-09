b6b953d1d1b9aa80219138dbb537c4520e260258
Charlie <charles.ae.dummett@outlook.com>
2023-03-09 16:14:52 +0000

    feat: add fuzz script

---
+++ scripts/run-docker-fuzz-test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+WORK_DIR="$(realpath "$(dirname "$0")/..")"
+RESULT_DIR="${WORK_DIR}/test_logs/$(date '+%F_%H%M%S')-integration"
+mkdir -p "${RESULT_DIR}"
+
+docker run \
+    --platform linux/amd64 \
+    -v "${RESULT_DIR}:/tmp" \
+    vega_sim_test:latest \
+        python -m vega_sim.scenario.adhoc -s fuzz_test
