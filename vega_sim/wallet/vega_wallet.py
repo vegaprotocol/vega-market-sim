e74a5d04f1927802991a443b1aac53d8ef2d0727
Tom McLean <tom@vegaprotocol.io>
2023-03-09 15:46:27 +0000

    feat: Update vega

--- vega_sim/wallet/vega_wallet.py
+++ vega_sim/wallet/vega_wallet.py
@@ -259,14 +259,7 @@ class VegaWallet(Wallet):
 
         url = f"{self.wallet_url}/api/v2/requests"
 
-        response = requests.post(url, headers=headers, json=submission)
-
-        try:
-            response.raise_for_status()
-        except:
-            import pdb
-
-            pdb.set_trace()
+        requests.post(url, headers=headers, json=submission).raise_for_status()
 
     def public_key(self, name: str, wallet_name: Optional[str] = None) -> str:
         """Return a public key for the given wallet name and key name.
