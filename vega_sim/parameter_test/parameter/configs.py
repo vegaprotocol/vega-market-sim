fe709ca80928c8ad20683f311252552155c23707
Charlie <charles.ae.dummett@outlook.com>
2023-03-22 17:39:49 +0000

    feat: update configs

--- vega_sim/parameter_test/parameter/configs.py
+++ vega_sim/parameter_test/parameter/configs.py
@@ -15,6 +15,67 @@ from vega_sim.parameter_test.parameter.experiment import (
     FILE_PATTERN_LOB,
 )
 
+MARGIN_SCALING_FACTORS = SingleParameterExperiment(
+    name="MarginScalingFactors",
+    parameter_type="network",
+    parameter_to_vary=("market.margin.scalingFactors"),
+    values=[
+        """{"search_level": 1.050, "initial_margin": 1.100, "collateral_release": 1.150}""",
+        """{"search_level": 1.001, "initial_margin": 2.000, "collateral_release": 4.000}""",
+    ],
+    scenario=ParameterExperiment(
+        num_steps=60 * 2,
+        step_length_seconds=1,
+        block_length_seconds=1,
+        state_extraction_fn=ideal_market_maker_single_data_extraction(
+            additional_data_fns=[
+                tau_scaling_additional_data,
+                target_stake_additional_data,
+                limit_order_book,
+            ]
+        ),
+        run_with_degen_agents=False,
+        wait_for_thread_catchup=True,
+    ),
+    runs_per_scenario=1,
+    additional_market_parameters_to_set={
+        "liquidity_monitoring_parameters.triggering_ratio": "1",
+    },
+    data_extraction=[
+        (FILE_PATTERN, BASE_IDEAL_MM_CSV_HEADERS),
+        (FILE_PATTERN_LOB, LOB_CSV_HEADERS),
+    ],
+)
+
+MARK_PRICE_UPDATE_FREQUENCY = SingleParameterExperiment(
+    name="MarkPriceUpdateFrequency",
+    parameter_type="network",
+    parameter_to_vary=("network.markPriceUpdateMaximumFrequency"),
+    values=["2s", "10s", "30s", "60s"],
+    scenario=ParameterExperiment(
+        num_steps=60 * 2,
+        step_length_seconds=1,
+        block_length_seconds=1,
+        state_extraction_fn=ideal_market_maker_single_data_extraction(
+            additional_data_fns=[
+                tau_scaling_additional_data,
+                target_stake_additional_data,
+                limit_order_book,
+            ]
+        ),
+        run_with_degen_agents=False,
+        wait_for_thread_catchup=True,
+    ),
+    runs_per_scenario=1,
+    additional_market_parameters_to_set={
+        "liquidity_monitoring_parameters.triggering_ratio": "1",
+    },
+    data_extraction=[
+        (FILE_PATTERN, BASE_IDEAL_MM_CSV_HEADERS),
+        (FILE_PATTERN_LOB, LOB_CSV_HEADERS),
+    ],
+)
+
 TARGET_STAKE_SCALING_FACTOR = SingleParameterExperiment(
     name="TargetStakeScalingFactor",
     parameter_type="market",
@@ -23,6 +84,7 @@ TARGET_STAKE_SCALING_FACTOR = SingleParameterExperiment(
     ),
     values=[0.5, 5, 50],
     scenario=ParameterExperiment(
+        num_steps=60 * 2,
         state_extraction_fn=ideal_market_maker_single_data_extraction(
             additional_data_fns=[
                 tau_scaling_additional_data,
@@ -93,4 +155,6 @@ CONFIGS = [
     TARGET_STAKE_SCALING_FACTOR,
     TAU_SCALING_FACTOR,
     BOND_PENALTY_PARAMETER,
+    MARGIN_SCALING_FACTORS,
+    MARK_PRICE_UPDATE_FREQUENCY,
 ]
