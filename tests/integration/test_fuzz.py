8fd51daef31134473909eacac14b58368529a478
Charlie <charles.ae.dummett@outlook.com>
2023-03-23 12:23:58 +0000

    feat: reduce test steps

--- tests/integration/test_fuzz.py
+++ tests/integration/test_fuzz.py
@@ -6,4 +6,4 @@ def test_fuzz_run():
     # Simply testing that it doesn't error
     import vega_sim.scenario.fuzzed_markets.run_fuzz_test as fuzz
 
-    fuzz._run(steps=100, output=False)
+    fuzz._run(steps=50, output=True)
