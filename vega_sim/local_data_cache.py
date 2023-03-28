37a3b3bb1cc6fddecb894bd1f3410402b4cb758d
Tom McLean <tom@vegaprotocol.io>
2023-03-28 18:45:03 +0100

    feat: Wait for catchup before initialising feed

--- vega_sim/local_data_cache.py
+++ vega_sim/local_data_cache.py
@@ -379,15 +379,15 @@ class LocalDataCache:
             ]
         with self.market_data_lock:
             for market_id in market_ids:
-                self.market_data_from_feed_store[market_id] = (
-                    data.get_latest_market_data(
-                        market_id,
-                        data_client=self._trading_data_client,
-                        market_price_decimals_map=self._market_price_decimals,
-                        market_position_decimals_map=self._market_pos_decimals,
-                        asset_decimals_map=self._asset_decimals,
-                        market_to_asset_map=self._market_to_asset,
-                    )
+                self.market_data_from_feed_store[
+                    market_id
+                ] = data.get_latest_market_data(
+                    market_id,
+                    data_client=self._trading_data_client,
+                    market_price_decimals_map=self._market_price_decimals,
+                    market_position_decimals_map=self._market_pos_decimals,
+                    asset_decimals_map=self._asset_decimals,
+                    market_to_asset_map=self._market_to_asset,
                 )
 
     def initialise_transfer_monitoring(
