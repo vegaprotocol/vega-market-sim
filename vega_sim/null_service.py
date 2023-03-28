e37f9188e346ee4ca2d4443625b05c57927d89ca
Tom McLean <tom@vegaprotocol.io>
2023-03-28 15:46:34 +0100

    feat: Add gRPC readiness check to criteria on startup

--- vega_sim/null_service.py
+++ vega_sim/null_service.py
@@ -19,10 +19,13 @@ from io import BufferedWriter
 from os import path
 from typing import Dict, List, Optional, Set
 
+import grpc
 import requests
 import toml
 from urllib3.exceptions import MaxRetryError
 
+import vega_sim.api.governance as gov
+import vega_sim.grpc.client as vac
 from vega_sim import vega_bin_path, vega_home_path
 from vega_sim.service import VegaService
 from vega_sim.tools.load_binaries import download_binaries
@@ -736,6 +739,17 @@ class VegaServiceNull(VegaService):
             started = False
             for _ in range(500):
                 try:
+                    channel = grpc.insecure_channel(
+                        self.data_node_grpc_url,
+                        options=(("grpc.enable_http_proxy", 0),),
+                    )
+                    grpc.channel_ready_future(channel).result(timeout=30)
+                    trading_data_client = vac.VegaTradingDataClientV2(
+                        self.data_node_grpc_url,
+                        channel=channel,
+                    )
+                    gov.get_blockchain_time(trading_data_client)
+
                     requests.get(
                         f"http://localhost:{self.data_node_rest_port}/time"
                     ).raise_for_status()
@@ -753,6 +767,7 @@ class VegaServiceNull(VegaService):
                     MaxRetryError,
                     requests.exceptions.ConnectionError,
                     requests.exceptions.HTTPError,
+                    grpc.RpcError,
                 ):
                     time.sleep(0.1)
             if not started:
