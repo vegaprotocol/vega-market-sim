6dc84ef3b1f155176dbc35f3d772ba4ec4dfa2b7
Tom McLean <tom@vegaprotocol.io>
2023-03-28 18:43:26 +0100

    feat: Wait for catchup before initialising feed

--- vega_sim/local_data_cache.py
+++ vega_sim/local_data_cache.py
@@ -192,13 +192,13 @@ class LocalDataCache:
         self,
         asset_id: str,
     ) -> vega_protos.assets.Asset:
-        return self._asset_from_feed.get(asset_id, None)
+        return self._asset_from_feed[asset_id]
 
     def market_from_feed(
         self,
         market_id: str,
     ) -> vega_protos.markets.Market:
-        return self._market_from_feed.get(market_id, None)
+        return self._market_from_feed[market_id]
 
     def market_data_from_feed(
         self,
@@ -207,7 +207,7 @@ class LocalDataCache:
         """
         Output market info.
         """
-        return self.market_data_from_feed_store.get(market_id, None)
+        return self.market_data_from_feed_store[market_id]
 
     def order_status_from_feed(
         self, live_only: bool = True
@@ -379,15 +379,15 @@ class LocalDataCache:
             ]
         with self.market_data_lock:
             for market_id in market_ids:
-                self.market_data_from_feed_store[
-                    market_id
-                ] = data.get_latest_market_data(
-                    market_id,
-                    data_client=self._trading_data_client,
-                    market_price_decimals_map=self._market_price_decimals,
-                    market_position_decimals_map=self._market_pos_decimals,
-                    asset_decimals_map=self._asset_decimals,
-                    market_to_asset_map=self._market_to_asset,
+                self.market_data_from_feed_store[market_id] = (
+                    data.get_latest_market_data(
+                        market_id,
+                        data_client=self._trading_data_client,
+                        market_price_decimals_map=self._market_price_decimals,
+                        market_position_decimals_map=self._market_pos_decimals,
+                        asset_decimals_map=self._asset_decimals,
+                        market_to_asset_map=self._market_to_asset,
+                    )
                 )
 
     def initialise_transfer_monitoring(
