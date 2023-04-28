adcf1655b4a4cf2aad0fa565efefb9e2c091617e
Charlie <charles.ae.dummett@outlook.com>
2023-04-28 14:49:43 +0100

    feat: remove unneeded fuzz plots

--- tests/integration/test_fuzz.py
+++ tests/integration/test_fuzz.py
@@ -1,9 +1,11 @@
 import pytest
+import tempfile
+
+import vega_sim.scenario.fuzzed_markets.run_fuzz_test as fuzz
 
 
 @pytest.mark.integration
 def test_fuzz_run():
     # Simply testing that it doesn't error
-    import vega_sim.scenario.fuzzed_markets.run_fuzz_test as fuzz
-
-    fuzz._run(steps=100, output=True)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        fuzz._run(steps=100, output=True, output_dir=temp_dir)
