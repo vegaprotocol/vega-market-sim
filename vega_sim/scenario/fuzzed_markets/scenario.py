d91c5b4888811f3dc09f72a3c87837e54be87837
Charlie <charles.ae.dummett@outlook.com>
2023-05-05 14:27:30 +0100

    feat: update degen LPs

--- vega_sim/scenario/fuzzed_markets/scenario.py
+++ vega_sim/scenario/fuzzed_markets/scenario.py
@@ -324,13 +324,27 @@ class FuzzingScenario(Scenario):
                 degenerate_liquidity_providers.append(
                     DegenerateLiquidityProvider(
                         wallet_name="DEGENERATE_LIQUIDITY_PROVIDERS",
-                        key_name=f"MARKET_{str(i_market).zfill(3)}_AGENT_{str(i_agent).zfill(3)}",
+                        key_name=f"HIGH_DEGEN_MARKET_{str(i_market).zfill(3)}_AGENT_{str(i_agent).zfill(3)}",
                         market_name=market_name,
                         asset_name=asset_name,
                         initial_asset_mint=1_000,
                         commitment_factor=0.7,
                         step_bias=0.1,
-                        tag=f"MARKET_{str(i_market).zfill(3)}_AGENT_{str(i_agent).zfill(3)}",
+                        tag=f"HIGH_DEGEN_MARKET_{str(i_market).zfill(3)}_AGENT_{str(i_agent).zfill(3)}",
+                    )
+                )
+
+            for i_agent in range(45):
+                degenerate_liquidity_providers.append(
+                    DegenerateLiquidityProvider(
+                        wallet_name="DEGENERATE_LIQUIDITY_PROVIDERS",
+                        key_name=f"LOW_DEGEN_MARKET_{str(i_market).zfill(3)}_AGENT_{str(i_agent).zfill(3)}",
+                        market_name=market_name,
+                        asset_name=asset_name,
+                        initial_asset_mint=1_000,
+                        commitment_factor=0.5,
+                        step_bias=0.1,
+                        tag=f"LOW_DEGEN_MARKET_{str(i_market).zfill(3)}_AGENT_{str(i_agent).zfill(3)}",
                     )
                 )
 
