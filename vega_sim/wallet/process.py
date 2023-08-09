3e5a1a1f8bab9e16d54d9391a9dde42fc1c1f28b
daniel1302 <daniel.1302@gmail.com>
2023-08-09 13:16:20 +0200

    feat: improve wallet check before run the wallet process

--- vega_sim/wallet/process.py
+++ vega_sim/wallet/process.py
@@ -10,15 +10,72 @@ def threaded(fn):
     return wrapper
 
 class VegaWallet:
+    default_bin_path = "vegawallet"
     logger = logging.getLogger("vegawallet")
 
-    def __init__(self, bin_path: str, network_name: str, token_file_path: str) -> None:
+    def __init__(self, bin_path: str, network_name: str, token_file_path: str, wallet_home: str) -> None:
         self.bin_path = bin_path
+        if bin_path is None:
+            self.bin_path = VegaWallet.default_bin_path
+        
         self.token_file_path = token_file_path
         self.network_name = network_name
+        self.wallet_home = wallet_home
+
         self.process = None
         self.process_thread = None
     
+    def check_wallet(self, wallet_name: str):
+        from shutil import which
+        from os.path import isdir, exists
+        
+        if which(self.bin_path) is None:
+            raise Exception("Wallet binary not found")
+        
+        if not self.wallet_home is None and not isdir(self.wallet_home):
+            raise Exception("Wallet home does not exists")
+        
+        if self.network_name is None:
+            raise Exception('Network name is none')
+        
+        if self.token_file_path is None:
+            raise Exception('Passphrase file is none')
+        
+        if not exists(self.token_file_path):
+            raise Exception('Passphrase file does not exists')
+        
+        if wallet_name is None:
+            raise Exception('Wallet name cannot be None for check function')
+        
+        wallet_args = self._wallet_args(("key", "list", "--wallet", wallet_name))
+        print(wallet_args)
+        process = subprocess.Popen(wallet_args, shell=True, stdout=subprocess.PIPE)
+        process.wait()
+        if process.poll() != 0:
+            raise Exception(f"The {wallet_name} wallet does not exist in the vegawallet")
+
+
+    def _wallet_args(self, command: tuple) -> tuple:
+        wallet_args = (
+            self.bin_path,
+        )
+        wallet_args = wallet_args + command
+        wallet_args = wallet_args + (
+            "service",
+            "run",
+            "--network",
+            self.network_name,
+            "--load-tokens",
+            "--automatic-consent",
+            "--tokens-passphrase-file",
+            self.token_file_path,
+        )
+        
+        if not self.wallet_home is None:
+            wallet_args = wallet_args + ("--home", self.wallet_home, )
+        
+        return wallet_args
+
 
     def run(self, check: bool = False) -> subprocess.CompletedProcess:
         """
@@ -29,17 +86,7 @@ class VegaWallet:
         if self.process != None:
             raise RuntimeError("Wallet is already running")
         
-        wallet_args = (
-            self.bin_path, 
-            "service",
-            "run",
-            "--network",
-            self.network_name,
-            "--load-tokens",
-            "--automatic-consent",
-            "--tokens-passphrase-file",
-            self.token_file_path,
-        )
+        wallet_args = self._wallet_args(("service", "run")) + ("--no-version-check", )
 
         self.process = subprocess.Popen(
             wallet_args,
@@ -54,12 +101,17 @@ class VegaWallet:
                 
         except subprocess.CalledProcessError as e:
             VegaWallet.logger.error(f"{str(e)}")
+        
+        retcode = self.process.poll()
+        if check and retcode:
+            raise subprocess.CalledProcessError(retcode, self.process.args)
+        return subprocess.CompletedProcess(self.process.args, retcode)
 
         
     @threaded
     def background_run(self):
         VegaWallet.logger.info("Running the wallet in the background")
-        self.run()
+        self.run(check=True)
         
 
     def __del__(self):
