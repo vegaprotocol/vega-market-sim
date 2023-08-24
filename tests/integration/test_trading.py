87c8362f6af59284000ee03bab8bb23825a4790c
Tom McLean <tom@vegaprotocol.io>
2023-08-24 18:59:19 +0100

    test: Fixing up some tests

--- tests/integration/test_trading.py
+++ tests/integration/test_trading.py
@@ -96,38 +96,8 @@ def test_submit_amend_liquidity(vega_service_with_market: VegaServiceNull):
 
     liq_provis = vega.party_liquidity_provisions(LIQ.name, market_id=market_id)
     assert len(liq_provis) == 1
-    for provis in [
-        liq_provis[0].sells[0].liquidity_order,
-        liq_provis[0].buys[0].liquidity_order,
-    ]:
-        assert provis.reference == vega_protos.vega.PeggedReference.PEGGED_REFERENCE_MID
-        assert provis.offset == "500"
-        assert provis.proportion == 1
+    assert liq_provis[0].fee == "0.001"
 
-    buy_specs = [
-        vega_protos.vega.LiquidityOrder(
-            reference=vega_protos.vega.PeggedReference.PEGGED_REFERENCE_MID,
-            offset="100",
-            proportion=2,
-        ),
-        vega_protos.vega.LiquidityOrder(
-            reference=vega_protos.vega.PeggedReference.PEGGED_REFERENCE_BEST_BID,
-            offset="500",
-            proportion=5,
-        ),
-    ]
-    sell_specs = [
-        vega_protos.vega.LiquidityOrder(
-            reference=vega_protos.vega.PeggedReference.PEGGED_REFERENCE_MID,
-            offset="500",
-            proportion=6,
-        ),
-        vega_protos.vega.LiquidityOrder(
-            reference=vega_protos.vega.PeggedReference.PEGGED_REFERENCE_BEST_ASK,
-            offset="20",
-            proportion=1,
-        ),
-    ]
     vega.wait_for_total_catchup()
     vega.submit_liquidity(
         LIQ.name,
@@ -141,48 +111,8 @@ def test_submit_amend_liquidity(vega_service_with_market: VegaServiceNull):
     liq_provis = vega.party_liquidity_provisions(LIQ.name, market_id=market_id)
 
     assert len(liq_provis) == 1
-
-    for provis, exp_provis in zip(liq_provis[0].sells, sell_specs):
-        assert provis.liquidity_order.reference == exp_provis.reference
-        assert provis.liquidity_order.offset == exp_provis.offset
-        assert provis.liquidity_order.proportion == exp_provis.proportion
-
-    for provis, exp_provis in zip(liq_provis[0].buys, buy_specs):
-        assert provis.liquidity_order.reference == exp_provis.reference
-        assert provis.liquidity_order.offset == exp_provis.offset
-        assert provis.liquidity_order.proportion == exp_provis.proportion
-
-    num_levels = 11
-    expected_bid_prices = [0.2995, 0.299, 0.25, 0.245, 0, 0, 0, 0, 0, 0, 0]
-    expected_bid_volumes = [334.0, 192.0, 1.0, 584.0, 0, 0, 0, 0, 0, 0, 0]
-    expected_ask_prices = [0.3005, 0.305, 0.35, 0.3502, 0, 0, 0, 0, 0, 0, 0]
-    expected_ask_volumes = [333.0, 563.0, 1.0, 82.0, 0, 0, 0, 0, 0, 0, 0]
-
-    book_state = vega.market_depth(market_id, num_levels=num_levels)
-    bid_prices = [level.price for level in book_state.buys] + [0] * max(
-        0, num_levels - len(book_state.buys)
-    )
-    bid_volumes = [level.volume for level in book_state.buys] + [0] * max(
-        0, num_levels - len(book_state.buys)
-    )
-    ask_prices = [level.price for level in book_state.sells] + [0] * max(
-        0, num_levels - len(book_state.sells)
-    )
-    ask_volumes = [level.volume for level in book_state.sells] + [0] * max(
-        0, num_levels - len(book_state.sells)
-    )
-
-    for price, exp_price in zip(bid_prices, expected_bid_prices):
-        assert price == exp_price
-
-    for vol, exp_vol in zip(bid_volumes, expected_bid_volumes):
-        assert vol == exp_vol
-
-    for price, exp_price in zip(ask_prices, expected_ask_prices):
-        assert price == exp_price
-
-    for vol, exp_vol in zip(ask_volumes, expected_ask_volumes):
-        assert vol == exp_vol
+    assert liq_provis[0].fee == "0.005"
+    assert liq_provis[0].commitment_amount == "20000000"
 
 
 @pytest.mark.integration
