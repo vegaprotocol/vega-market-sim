5065db1de5ee12c70451e5ff4c10569f5f4075d2
Tom McLean <tom@vegaprotocol.io>
2023-10-04 17:44:46 +0100

    feat: Convert live price listener to websockets

--- vega_sim/scenario/common/utils/price_process.py
+++ vega_sim/scenario/common/utils/price_process.py
@@ -192,11 +192,8 @@ def _price_listener(iter_obj, symbol):
         f"wss://stream.binance.com:9443/ws/{symbol}@kline_1s",
         # on_open=lambda ws: print("ok"),
         on_message=lambda _, msg: _on_message(iter_obj, msg),
-        # on_error=on_error,
-        # on_close=on_close,
     )
     ws.run_forever(reconnect=5)
-    # self._forwarding_thread.start()
 
 
 def _on_message(iter_obj, message):
