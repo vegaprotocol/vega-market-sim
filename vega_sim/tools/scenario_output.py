a536c8d226e656e913ef1fa23b0b488862c28c41
Tom <tom@vegaprotocol.io>
2023-10-27 16:37:25 +0100

    feat: improve fuzzing memory usage (#539)

--- vega_sim/tools/scenario_output.py
+++ vega_sim/tools/scenario_output.py
@@ -1,13 +1,15 @@
-from vega_sim.environment.agent import Agent, StateAgentWithWallet
-from vega_sim.scenario.common.agents import MarketHistoryData, ResourceData
-from typing import List, Callable, Optional, Dict
-import pandas as pd
+import csv
 import itertools
+import json
 import os
 import os.path
-import json
+from typing import Callable, Dict, List, Optional
+
+import pandas as pd
 
 import vega_sim.proto.vega as vega_protos
+from vega_sim.environment.agent import Agent, StateAgentWithWallet
+from vega_sim.scenario.common.agents import MarketHistoryData, ResourceData
 
 DEFAULT_PATH = "./run_logs"
 DEFAULT_RUN_NAME = "latest"
@@ -39,121 +41,107 @@ def resource_data_to_row(data: ResourceData):
 
 
 def history_data_to_row(data: MarketHistoryData) -> List[pd.Series]:
-    results = []
     for market_id in data.market_info.keys():
         market_data = data.market_data[market_id]
 
-        results.append(
-            {
-                "time": data.at_time,
-                "mark_price": market_data.mark_price,
-                "market_id": market_id,
-                "mark_price": market_data.mark_price,
-                "mid_price": market_data.mid_price,
-                "open_interest": market_data.open_interest,
-                "best_bid": market_data.best_bid_price,
-                "best_offer": market_data.best_offer_price,
-                "best_bid_volume": market_data.best_bid_volume,
-                "best_offer_volume": market_data.best_offer_volume,
-                "market_state": market_data.market_state,
-                "market_trading_mode": market_data.market_trading_mode,
-                "target_stake": market_data.target_stake,
-                "supplied_stake": market_data.supplied_stake,
-                "price_monitoring_bounds": [
-                    ((bound.min_valid_price, bound.max_valid_price))
-                    for bound in market_data.price_monitoring_bounds
-                ],
-                "indicative_price": market_data.indicative_price,
-                "trigger": market_data.trigger,
-                "extension_trigger": market_data.extension_trigger,
-            },
-        )
-    return results
+        yield {
+            "time": data.at_time,
+            "mark_price": market_data.mark_price,
+            "market_id": market_id,
+            "mark_price": market_data.mark_price,
+            "mid_price": market_data.mid_price,
+            "open_interest": market_data.open_interest,
+            "best_bid": market_data.best_bid_price,
+            "best_offer": market_data.best_offer_price,
+            "best_bid_volume": market_data.best_bid_volume,
+            "best_offer_volume": market_data.best_offer_volume,
+            "market_state": market_data.market_state,
+            "market_trading_mode": market_data.market_trading_mode,
+            "target_stake": market_data.target_stake,
+            "supplied_stake": market_data.supplied_stake,
+            "price_monitoring_bounds": [
+                ((bound.min_valid_price, bound.max_valid_price))
+                for bound in market_data.price_monitoring_bounds
+            ],
+            "indicative_price": market_data.indicative_price,
+            "trigger": market_data.trigger,
+            "extension_trigger": market_data.extension_trigger,
+        }
 
 
-def history_data_to_order_book_rows(data: MarketHistoryData) -> List[pd.Series]:
-    results = []
+def history_data_to_order_book_rows(data: MarketHistoryData) -> List[dict]:
     for market_id, depth in data.market_depth.items():
         for side, side_vals in [("BID", depth.buys), ("ASK", depth.sells)]:
             for i, level_data in enumerate(side_vals):
-                results.append(
-                    {
-                        "time": data.at_time,
-                        "side": side,
-                        "price": level_data.price,
-                        "volume": level_data.volume,
-                        "level": i,
-                        "market_id": market_id,
-                    }
-                )
-    return results
+                yield {
+                    "time": data.at_time,
+                    "side": side,
+                    "price": level_data.price,
+                    "volume": level_data.volume,
+                    "level": i,
+                    "market_id": market_id,
+                }
 
 
-def history_data_to_trade_rows(data: MarketHistoryData) -> List[pd.Series]:
-    results = []
+def history_data_to_trade_rows(data: MarketHistoryData) -> List[dict]:
     for market_id, trades in data.trades.items():
         for trade in trades:
-            results.append(
-                {
-                    "time": trade.timestamp,
-                    "seen_at": data.at_time,
-                    "id": trade.id,
-                    "price": trade.price,
-                    "size": trade.size,
-                    "buyer": trade.buyer,
-                    "seller": trade.seller,
-                    "aggressor": trade.aggressor,
-                    "buy_order": trade.buy_order,
-                    "sell_order": trade.sell_order,
-                    "market_id": market_id,
-                    "trade_type": trade.trade_type,
-                    "buyer_fee_infrastructure": trade.buyer_fee.infrastructure_fee,
-                    "buyer_fee_liquidity": trade.buyer_fee.liquidity_fee,
-                    "buyer_fee_maker": trade.buyer_fee.maker_fee,
-                    "seller_fee_infrastructure": trade.seller_fee.infrastructure_fee,
-                    "seller_fee_liquidity": trade.seller_fee.liquidity_fee,
-                    "seller_fee_maker": trade.seller_fee.maker_fee,
-                    "buyer_auction_batch": trade.buyer_auction_batch,
-                    "seller_auction_batch": trade.seller_auction_batch,
-                }
-            )
-    return results
-
-
-def history_data_to_account_rows(data: MarketHistoryData) -> List[pd.Series]:
-    results = []
-    for account in data.accounts:
-        results.append(
-            {
-                "time": data.at_time,
-                "party_id": account.owner,
-                "balance": account.balance,
-                "market_id": account.market_id,
-                "asset": account.asset,
-                "type": account.type,
+            yield {
+                "time": trade.timestamp,
+                "seen_at": data.at_time,
+                "id": trade.id,
+                "price": trade.price,
+                "size": trade.size,
+                "buyer": trade.buyer,
+                "seller": trade.seller,
+                "aggressor": trade.aggressor,
+                "buy_order": trade.buy_order,
+                "sell_order": trade.sell_order,
+                "market_id": market_id,
+                "trade_type": trade.trade_type,
+                "buyer_fee_infrastructure": trade.buyer_fee.infrastructure_fee,
+                "buyer_fee_liquidity": trade.buyer_fee.liquidity_fee,
+                "buyer_fee_maker": trade.buyer_fee.maker_fee,
+                "seller_fee_infrastructure": trade.seller_fee.infrastructure_fee,
+                "seller_fee_liquidity": trade.seller_fee.liquidity_fee,
+                "seller_fee_maker": trade.seller_fee.maker_fee,
+                "buyer_auction_batch": trade.buyer_auction_batch,
+                "seller_auction_batch": trade.seller_auction_batch,
             }
-        )
-    return results
+
+
+def history_data_to_account_rows(data: MarketHistoryData) -> List[dict]:
+    for account in data.accounts:
+        yield {
+            "time": data.at_time,
+            "party_id": account.owner,
+            "balance": account.balance,
+            "market_id": account.market_id,
+            "asset": account.asset,
+            "type": account.type,
+        }
 
 
 def _market_data_standard_output(
     market_history_data: List[MarketHistoryData],
     file_name: str,
     output_path: str = DEFAULT_PATH,
-    data_to_row_fn: Callable[[MarketHistoryData], pd.Series] = history_data_to_row,
+    data_to_row_fn: Callable[[MarketHistoryData], dict] = history_data_to_row,
 ):
-    results = list(
-        itertools.chain.from_iterable(
-            [data_to_row_fn(data=step_data) for step_data in market_history_data],
-        )
-    )
-    result_df = pd.DataFrame.from_records(
-        results,
-        index="time" if len(results) > 0 else None,
-    )
-    os.makedirs(output_path, exist_ok=True)
+    if len(market_history_data) == 0:
+        return
 
-    result_df.to_csv(path_or_buf=os.path.join(output_path, file_name))
+    os.makedirs(output_path, exist_ok=True)
+    with open(os.path.join(output_path, file_name), "w") as f:
+        writer = None
+
+        for step_data in market_history_data:
+            for row_data in data_to_row_fn(data=step_data):
+                if row_data:
+                    if writer is None:
+                        writer = csv.DictWriter(f, fieldnames=list(row_data.keys()))
+                        writer.writeheader()
+                    writer.writerow(row_data)
 
 
 def market_data_standard_output(
