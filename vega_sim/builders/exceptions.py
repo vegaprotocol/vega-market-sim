22eeb3c5b6d39d304c099496f2e15458a092330b
Charlie <charlie@vegaprotocol.io>
2023-11-14 11:58:10 +0000

    feat: wrap function

--- vega_sim/builders/exceptions.py
+++ vega_sim/builders/exceptions.py
@@ -1,16 +1,18 @@
 import logging
 
 from typing import Callable
+from functools import wraps
 
 
 def raise_custom_build_errors(func: Callable):
-    def inner(*args, **kwargs):
+    @wraps(func)
+    def wrapped_fn(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except ValueError as ve:
             raise VegaProtoValueError(ve)
 
-    return inner
+    return wrapped_fn
 
 
 class VegaProtoValueError(ValueError):
