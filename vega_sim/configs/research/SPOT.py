3047e220aaaf72bc9348a13cdac923b67cd3a8be
Charlie <charlie@vegaprotocol.io>
2024-03-19 10:51:32 +0000

    feat: support spot market configurations

---
+++ vega_sim/configs/research/SPOT.py
@@ -0,0 +1,65 @@
+from vega_sim.api.market import SpotMarketConfig
+
+CONFIG = SpotMarketConfig(
+    {
+        "decimalPlaces": "2",
+        "positionDecimalPlaces": "4",
+        "tickSize": "10",
+        "instrument": {
+            "name": "Bitcoin / Tether USD (Spot)",
+            "code": "BTC/USDT",
+            "spot": {
+                "baseAsset": None,
+                "quoteAsset": None,
+                "name": "BTC/USDT",
+            },
+        },
+        "metadata": [
+            "base:BTC",
+            "quote:USDT",
+            "oracle:pyth",
+            "oracleChain:gnosis",
+            "class:fx/crypto",
+            "perpetual",
+            "sector:defi",
+            "enactment:2023-12-01T18:00:00Z",
+        ],
+        "priceMonitoringParameters": {
+            "triggers": [
+                {
+                    "horizon": "21600",
+                    "probability": "0.9999999",
+                    "auctionExtension": "86400",
+                },
+                {
+                    "horizon": "4320",
+                    "probability": "0.9999999",
+                    "auctionExtension": "3600",
+                },
+                {
+                    "horizon": "1440",
+                    "probability": "0.9999999",
+                    "auctionExtension": "1800",
+                },
+                {
+                    "horizon": "360",
+                    "probability": "0.9999999",
+                    "auctionExtension": "300",
+                },
+            ]
+        },
+        "targetStakeParameters": {"timeWindow": "3600", "scalingFactor": 0.05},
+        "logNormal": {
+            "riskAversionParameter": 0.000001,
+            "tau": 0.000003995,
+            "params": {"mu": 0, "r": 0, "sigma": 1},
+        },
+        "liquiditySlaParameters": {
+            "priceRange": "0.03",
+            "commitmentMinTimeFraction": "0.75",
+            "performanceHysteresisEpochs": "1",
+            "slaCompetitionFactor": "0.8",
+        },
+        "liquidityFeeSettings": {"method": "METHOD_MARGINAL_COST"},
+    },
+)
