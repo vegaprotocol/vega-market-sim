18885b5290841d7fa162faa80d505f7a353f4ac3
Charlie <charlie@vegaprotocol.io>
2024-08-27 17:01:48 +0100

    feat: add vega_protos tests

---
+++ tests/vega_protos/test_protos.py
@@ -0,0 +1,15 @@
+import vega_protos as protos
+
+
+def test_data_node_attributes():
+    protos.data_node.api.v2.trading_data
+
+
+def test_vega_attributes():
+    protos.vega.assets
+    protos.vega.chain_events
+    protos.vega.data_source
+    protos.vega.governance
+    protos.vega.markets
+    protos.vega.oracle
+    protos.vega.vega
